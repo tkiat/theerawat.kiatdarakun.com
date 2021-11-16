ff12fcbbf3f437a4afd49209c0d4cd95246b4501
Theerawat Kiatdarakun <tkiat@tutanota.com>
2021-11-16 22:17:57 +0700

    content: update story

--- src/App/component/Content/page/Story/MasterDegree.tsx
+++ src/App/component/Content/page/Story/MasterDegree.tsx
@@ -5,14 +5,51 @@ import {MyReactTooltip} from "../../share/MyReactTooltip"
 export const MasterDegree = (): React.ReactElement =>
   <>
     <h1>24 - 26 Years</h1>
-    <p>I was still very ignorant about the direction in life but I felt that pursuing a Master's degree abroad is the best investment to do. Apart from the Cambodia border, I hadn't been outside of my country before. I sent the applications to RWTH Aachen and TU Muenchen in Germany (base on the ranking which was actually quite useless because many German universities have high standard). The one I got accepted was RWTH Aachen. This meant I didn't have to pay any tuition fees except the&nbsp;
-    <MyReactTooltip id="tt-s-rwthfee" text="social contribution fee">
-      <p>It costed me around 220 EUR per semester. This granted me free transport (bus, train) within the Nordrhein-Westfalen. The area was quite big which includes Duesseldorf, Cologne, and Dortmund. I also could take the university cafeteria which was usually cheaper than eating outside.</p>
-    </MyReactTooltip>
-    . The application for RWTH Aachen required a bit of&nbsp;
-    <MyReactTooltip id="tt-s-rwthprep" text="preparation">
-      <p>I needed to send my IELTS scores and also open letters of recommendations. The annoying part was that they also require me taking GRE (which costed around 180 USD). I was so lazy that I took only one day to prepare it. The score was not nice except the qualitative section.</p>
-    </MyReactTooltip>
-    &nbsp;but I did it anyway.</p>
-    TODO
+    <div>I was still very ignorant about the direction in life but I felt that pursuing a Master's degree abroad was the best investment to do. And apart from the Cambodia border, I hadn't been outside of my country before. I sent the applications to RWTH Aachen and TU Muenchen in Germany because of the&nbsp;
+      <MyReactTooltip id="tt-s-ranking" text="ranking">
+        This was actually quite useless because many German universities have high standard. The area of study and thesis topics are more important in my opinion.
+      </MyReactTooltip>
+      . The one I got accepted was RWTH Aachen. This meant I didn't have to pay any tuition fees except the&nbsp;
+      <MyReactTooltip id="tt-s-rwthfee" text="social contribution fee">
+        It costed me around 220 EUR per semester. This granted me free transport (bus, train) within the Nordrhein-Westfalen. The area was quite big which includes Duesseldorf, Cologne, and Dortmund. I also could take the university cafeteria which was usually cheaper than eating outside.
+      </MyReactTooltip>
+      . The application for RWTH Aachen required a bit of&nbsp;
+      <MyReactTooltip id="tt-s-rwthprep" text="preparation">
+        I needed to send my IELTS scores and also open letters of recommendations. The annoying part was that they also require me taking GRE (which costed around 180 USD). I was so lazy that I took only one day to prepare it. The score was not nice except the qualitative section. I had to send applications via mail also.
+      </MyReactTooltip>
+      &nbsp;but I did it anyway.
+    </div><br/>
+    <div>
+      On the first day of arrival, I had the appointment with&nbsp;
+      <MyReactTooltip id="tt-s-twostudents" text="two kind Thai students">
+        One met me at Cologne and one took a taxi with me from Aachen Hofbahnhof (train station) to my room. I just met an unknown German lady (probably like 50 year old) who stood with me at Hofbahnhof because she said it was dangerous (not true in my opinion).
+      </MyReactTooltip>
+      &nbsp;who will take me to my single-room apartment. I adapted quickly to a new&nbsp;
+      <MyReactTooltip id="tt-s-lifestyle" text="lifestyle">
+        <p>where I have to cook myself. I always bought pre-made food in Thailand but it was prohibitive to buy them in Germany (it's like 1 vs 5 EUR). The average meal cost in Germany is 1.5 EUR only if I cook myself.</p>
+        <p>The climate was quite the opposite of Thailand where I had to heat my room instead of cooling it. My cloth needed to be thick during the cold winter when I could sometimes expect snow.</p>
+      </MyReactTooltip>
+      , went to&nbsp;
+      <MyReactTooltip id="tt-s-drilander" text="Dreilaenderpunkt">
+        <p>It is simply the meeting point of three countries: Germany, The Netherlands, and Belgium. It took me around 1.5 hours by foot from my apartment. The view was nice and the path to it had some farming and horses. The experience was totally priceless. I visited this place many times (&gt; 5).</p>
+      </MyReactTooltip>
+      , and finally joined the&nbsp;
+      <MyReactTooltip id="tt-s-orientation" text="orientation">
+        This was when I realized more than half was Indian. A very little chinese was here in my batch which was a little surprising. There was only one German. If you want to meet German in class, take a class in German, not in English.
+      </MyReactTooltip>
+      . The&nbsp;
+      <MyReactTooltip id="tt-s-firstsem" text="first semester">
+        <p>I took all the courses as many as possible because I didn't have anything better to do. The exam here was taken only one time at the end of the semester and very short (15 minute oral exam or 1.5 hours written exam).</p>
+        <p>In my opinion, if you believe you cannot process the exam quickly, don't come. You will be rejected from the university if you fail the exam of the same course three times in a row.</p>
+      </MyReactTooltip>
+      &nbsp;passed quickly. The second semester was really interesting to me as I will share the same&nbsp;
+      <MyReactTooltip id="tt-s-anilroom" text="room">
+        It costed me only around 210 EUR including internet, water, and electrity unlike 360 EUR in the previous apartment. And the best of all, it has balcony! Drinking beer over there during the summer was like a heaven!
+      </MyReactTooltip>
+      &nbsp;as a fellow indian batchmate from Karnataka, India. The&nbsp;
+      <MyReactTooltip id="tt-s-thirdsem" text="third semester">
+        <p>TODO</p>
+      </MyReactTooltip>
+      &nbsp;was
+    </div>
   </>
