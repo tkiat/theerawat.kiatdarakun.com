a99e72781ee68bb8591beebf3c6c29510f429ebe
Theerawat Kiatdarakun <tkiat@tutanota.com>
2021-11-17 17:18:56 +0700

    content: modify story

--- src/App/component/Content/page/Story/LongUnemployment.tsx
+++ src/App/component/Content/page/Story/LongUnemployment.tsx
@@ -1,11 +1,20 @@
 import React from "react"
 
-// import {MyReactTooltip} from "../../share/MyReactTooltip"
+import {MyReactTooltip} from "../../share/MyReactTooltip"
 
 export const LongUnemployment = (): React.ReactElement =>
   <>
     <h1>28 Years - Present</h1>
-    <p>I still didn't know what was going on in my life during this long unemployment except one thing: electricity cost. I didn't use an air-conditioner but I had a self-assembled PC with about 450W PSU. This was not much by the desktop standard but it was too prohibitive compared to a laptop. I also didn't like when it went off immediately after the power shortage occured. I could buy a power backup but it was too clunky.</p>
-    <p>My old laptop that I purchased in 2008 came to my mind. It was VAIO VGNCS16S that costed me about 1000 USD. That was quite a lot of money but I liked it beauty (my only criterion at the time). With the notorious Microsoft Vista installed, it took me ages to boot successfully. I first replaced Vista with Ubuntu, the only Linux distribution I knew at the time. </p>
-    TODO
+    <p>My mind became blank after coming back to Thailand. The sky train near my house was finished. A new shopping mall emerged. Many things seemed to change yet nothing was really changed. Something fundamental always stays the same, people still enjoy and suffer. Many things that I didn't consider useful still engrossed so many people. I applied to only three jobs but didn't pass the interviews. This coupled with the fact that I just bought a new Ikea table and a new 27-inch monitor, video games became my new full-time job. It was an illusion, I was a mere consumer who didn't produce anything. After wasting life for more than one year, I noticed that my self-assembled PC incurred much electricity cost and was also prone to the occasional power shortage.</p>
+    <div>
+      My very old&nbsp;
+      <MyReactTooltip id="tt-s-laptopvaio" text="laptop">
+        It was VAIO VGNCS16S that cost me (to be precise, my father) about 1000 USD in 2008. That was quite a lot of money but I liked its beauty (my only criterion at the time). With the notorious Microsoft Vista still installed, it took me ages to boot successfully.
+      </MyReactTooltip>
+      &nbsp;came to my mind. I first replaced Vista with Ubuntu and Void Linux. The Void Linux was performant and that was an eye-opener for me. I became interested in this liberating piece of software (Linux) and abandoned my PC. I sold my PC afterward and bought two used&nbsp;
+      <MyReactTooltip id="tt-s-laptopthinkpad" text="Thinkpad X200">
+        They allowed me to install open-source WiFi firmware and bootloader (Libreboot then coreboot). Previously, the proprietary solution might contain spyware because no one can see the code.
+      </MyReactTooltip>
+      . I deleted my video game accounts. I sold my Ipad and bought new cheaper phones. I quit buying freezer food and chips from 7-11 (a convenience store). I valued something lean and liberating more than something convenient and costly (in terms of money or environment). I read a few philosophy books. Now, I believe I am much wiser than I ever was before. I appreciate the given opportunity to introspect and now I have a clearer purpose to pursue.
+    </div>
   </>
--- src/App/component/Content/page/Story/WorkingGermany.tsx
+++ src/App/component/Content/page/Story/WorkingGermany.tsx
@@ -5,83 +5,101 @@ import {MyReactTooltip} from "../../share/MyReactTooltip"
 export const WorkingGermany = (): React.ReactElement =>
   <>
     <h1>26 - 28 Years</h1>
-    <div>Just before the end of my thesis I applied to only to&nbsp;
+    <div>Just before the end of my thesis I only applied to&nbsp;
       <MyReactTooltip id="tt-s-nidresden" text="National Instruments Dresden">
         A company in the east Germany acquired by National Instruments some time ago. It offers a testing solution mainly in the telecommunications area. People over there could speak German and also had German-like faces except one guy and some interns only.
       </MyReactTooltip>
     &nbsp;and got the 6-month&nbsp;
       <MyReactTooltip id="tt-s-internoffer" text="offer">
-        All internships in Germany were required to be paid by law. The stipend ranged from 550 EUR to 1700 EUR from what I had heard. The interns in the west and south generally paid more than the east but the living cost is also higher. Obviously, it was harder to get one if you don't have a working German proficiency but you should be able to find it nonetheless with some efforts.
+        All internships in Germany were required to be paid by law. The stipend ranged from 550 EUR to 1700 EUR from what I had heard. The interns in the west and south generally paid more than the east but the living cost is also higher. Obviously, it was harder to get one if you don't have a working German proficiency but you should be able to find it nonetheless with some effort.
       </MyReactTooltip>
     . There were three other fellow&nbsp;
       <MyReactTooltip id="tt-s-fellowinterns" text="interns">
-        My batchmate and also a short-term roommate. The other two come from Columbia and Egypt. All studied here in Germany (of course).
+        My batchmate and also a short-term roommate. The other two come from Columbia and Egypt. All studied here in Germany (of course). Another batchmate of mine got a full-time job here also at the time I took an internship.
       </MyReactTooltip>
-      &nbsp;here. The company has some&nbsp;
+      &nbsp;here who went together places nearby such as&nbsp;
+      <MyReactTooltip id="tt-s-internprague" text="Prague">
+        <p>We booked a cheap hostel (10 EUR each) and in the night we visited a bar. There was some dancing lady with a lingerie who made her piece tighter after I stared at her. I felt sleepy but the guard prevented me from doing so and told me to sleep at my home instead. I never understand why would someone wanted to visit such a place.</p>
+        <p>We went to a very old restaurant in Prague where we were served big poultry food and beer. We went to a castle to inspect the heritage of something old I actually didn't know anything about. The beer was nice though.</p>
+      </MyReactTooltip>
+      . The company has some&nbsp;
       <MyReactTooltip id="tt-s-perks" text="perks">
         Just free sugary drinks, coffee, and tea.
       </MyReactTooltip>
       &nbsp;but the best thing was&nbsp;
       <MyReactTooltip id="tt-s-buffets" text="buffets">
-        One costs only 5 EUR and there was basically all you can eat including chickens, rice, and some soup. Another restaurant had similar meals but limited to a given container.
+        One costs only 5 EUR and there was basically all you can eat including chickens, rice, and some soup. Another restaurant had similar meals but was limited to a given container.
       </MyReactTooltip>
       &nbsp;in the Vietnamese restaurants nearby. I carried out the internship&nbsp;
       <MyReactTooltip id="tt-s-tasks" text="tasks">
-        Basically some backlogs that engineers over there still didn't complete.
+        Basically, some backlogs that engineers over there still didn't complete.
       </MyReactTooltip>
       &nbsp;day by day under the&nbsp;
       <MyReactTooltip id="tt-s-supervision" text="supervision">
         There was a regular 30-minute two-person meeting at the beginning of Monday which was later increased to 1 hour. We wrote the plan all over the whiteboard and talked about the result. That was awesome.
       </MyReactTooltip>
-      &nbsp;of a German engineer until it was finally the end of the internship when I was given a textbook but never read. I felt like I couldn't help those awesome German engineers much but I still wanted to stay there so I moved to nearby&nbsp;
+      &nbsp;of a German engineer until it was finally the end of the internship when I was given a textbook but never read. I felt like I couldn't&nbsp;
+      <MyReactTooltip id="tt-s-helpeng" text="help">
+        I felt my personality didn't match with those engineers and the culture. I didn't want to change myself too much.
+      </MyReactTooltip>
+      &nbsp;those awesome German engineers much but I still wanted to stay there so I moved to nearby&nbsp;
       <MyReactTooltip id="tt-s-dresdenapartments" text="apartments">
-        The first apartment is a one (or two I don't remember) stay with some random russian people. My mother visited me shortly here and we went together in Prague. The second apartment I lives with German and French students where I knew that Crepe was way simpler than the one in Thailand.
+        The first apartment is a one (or two I don't remember) stay with some random Russian people. My mother visited me shortly here and we went together to Prague. In the second apartment, I lived with German and French students where I knew that Crepe was way simpler than the one in Thailand.
       </MyReactTooltip>
       .
     </div>
     <br />
     <div>
-      I was totally confused about the direction in life so I decided to find jobs in Berlin because it was my dream city to live in. I managed to find an&nbsp;
+      I was totally confused about the direction in life so I decided to find a job in Berlin because it was my dream city to live in. I managed to find an&nbsp;
       <MyReactTooltip id="tt-s-berlinapartment1" text="expensive one">
-        It cost a whopping 600 EUR per month. It was very big and for two people actually but I had no other offer because they seemed prefer want a long-term stay. I intended to find jobs over here but this was indeed a crazy decision.
+        It cost a whopping 600 EUR per month. It was very big and for two people but I had no other offer because they seemed to prefer a long-term stay. I intended to find jobs over here but this was indeed a crazy decision.
       </MyReactTooltip>
       . But I lost&nbsp;
       <MyReactTooltip id="tt-s-berlinlost" text="my luggage">
-        A bag containing a lot like 600 EUR, tablet, laptop, and many other things. This complicated the issue since the houseowner wanted the money right away. At that time, my mother was married to a guy in Norway and he very kindly gave (lend) me some money like 2000 EUR.
+        A bag containing a lot like 600 EUR, tablet, laptop, and many other things. This complicated the issue since the house owner wanted the money right away. At that time, my mother was married to a guy in Norway and he very kindly gave (lend) me some money like 2000 EUR. I contacted the police about this incident but no one contacted me back.
       </MyReactTooltip>
-      &nbsp;when I first reached Berlin train station. After that, an&nbsp;
-      <MyReactTooltip id="tt-s-berlinincident" text="incident">
-        I visited the Buddhavihara temple in Berlin in a hope to find another accommodation or at least someone to share a room with. and after an indicent a nun gave me and another Thai
+      &nbsp;when I first reached Berlin train station. After this&nbsp;
+      <MyReactTooltip id="tt-s-templeincident" text="incident">
+        I visited the Buddhavihara temple in Berlin in the hope to find another accommodation or at least someone to share a room with. I found somebody but still didn't reach a conclusion. The next day I accidentally met the abbot of that temple and he remembered me. I went with him to have some launch and then he suggested a nun who had an available apartment.
       </MyReactTooltip>
-      &nbsp;allowed to stay very&nbsp;
+      , I stayed at a very&nbsp;
       <MyReactTooltip id="tt-s-berlinapartmentcheap" text="cheap">
-        It cost only 200 EUR per month. She said she has rented it since around 30 years ago and the price remains low. The people live in three separate rooms. The kitchen was nicely furnished.
+        It cost only 200 EUR per month. She said she has rented it since around 30 years ago and the price remains low. The people live in three separate rooms. The kitchen was nicely furnished. The location was unbelievably convenient since there was a regular Turkish market just in front of the apartment and plenty of shops nearby.
       </MyReactTooltip>
-      &nbsp;with a Thai nun and another roommate. I took a plunge and enrolled two german&nbsp;
+      &nbsp;place with a Thai nun (who seldom stayed) and another roommate. I took a plunge and enrolled two german&nbsp;
       <MyReactTooltip id="tt-s-berlingermancourses" text="courses">
-        An B2 level German and also a speaking course. It was on of the best experience ever since I passed Brandenburger Tor very often by bicycle. I felt I was a Berliner.
+        A B2 level German and also a speaking course. It was one of the best experiences ever since I passed Brandenburger Tor very often by bicycle. I felt I was a Berliner. I considered the course price to be cheap, at least cheaper than the one from Geothe Institute in Thailand.
       </MyReactTooltip>
-      . I applied and decided to get a&nbsp;
+      . After a while, I got a&nbsp;
       <MyReactTooltip id="tt-s-berlinjob" text="job">
-        <p>A job that valued Thai ability at Arvato Bertelsmann. I so far hadn't apply to any enginering job in Germany partly because I felt they don't need me except for cleaning and restaurant jobs but got rejected. I thought I would be needed there because I could speak Thai.</p>
-        <p>A job is more like social science related, not engineering which is fine as I don't care much. I declined the full-time position and took 6 hours shift first. 9 months later, I converted to a full-time worker.</p>
+        <p>A job that valued Thai ability at Arvato Bertelsmann. I so far hadn't applied to any engineering job in Germany partly because I felt they don't need me except for cleaning and restaurant jobs but got rejected. I thought I would be needed there because I could speak Thai.</p>
+        <p>A job is more social-science related, not engineering at all which was fine as I didn't care much. I declined the full-time position and took part-time for 30 hours per week first. 9 months later, I converted to a full-time worker.</p>
       </MyReactTooltip>
       &nbsp;around 8km from my apartment. I invited my father&nbsp;
       <MyReactTooltip id="tt-s-berlinfather" text="here">
-        <p>I paid everything for this visit including a two-way plane ticket. My father was amazing by a number of beers here. He like them a lot and drank a lot each day.</p>
-        <p>Then we went to Prague and ate the same restaurant and visited the same castle as I did with my fellow interns but my father couldn't walk too much as he felt tired.</p>
+        <p>I paid everything for this visit including a two-way plane ticket. My father was amazed by the number of beers here. He like them a lot and drank a lot each day.</p>
+        <p>Then we went to Prague. The hostel we booked had no free place anymore so we got the private room for free! As I did with my fellow interns, we ate at the same restaurant and visited the same castle but my father felt really tired after the trip. I took my father for a beer in a bar in Prague which became his most favorite beer ever.</p>
       </MyReactTooltip>
-      &nbsp;in March, 2017. The job experience was unbelievable and new. However, the&nbsp;
+      &nbsp;in March, 2017. The job&nbsp;
+      <MyReactTooltip id="tt-s-arvatoexperience" text="experience">
+        <p>Just sit in front of a computer for a long time. The company has some perks like a half-price train ticket but I rejected that because I like my bicycle. I, therefore, rode a bicycle alone on the way back. It was the best ever experience to put a beer from the supermarket and then became very cold naturally after I reached my place.</p>
+        <p>Half of the time I expected to join a night shift and finished at 10 pm. It was dark and cold on the way back but not that much. My Jack Wolfskin jacket was always reliable.</p>
+      </MyReactTooltip>
+      &nbsp; was unbelievable and new. However, the&nbsp;
       <MyReactTooltip id="tt-s-berlinmonotony" text="monotony">
-        <p>Something in me changed. At first I thought I can have good work and continue until I die. The ultimate purpose in life in my mind at the time was happiness but I still didn't know what it was. I finally felt the struggling German people had.</p>
-        <p>They usually pay for non-trivial amount of rent and also non-trivial amount of tax and perhaps a health insurance. They usually have pressure to find and retain the job or else they will face an unemployment nightmare. I give a deepest sympathy to homeless specifically during in the winter.</p>
-        <p>I concluded that the life without rent and without much pressure is a better life.</p>
+        <p>Something in me changed. At first, I thought I can have good work and continue until I die. The ultimate purpose in life in my mind at the time was happiness but I still didn't know what it was. I finally felt the struggling German people had.</p>
+        <p>They usually pay for a non-trivial amount of rent and tax and perhaps also the health insurance. They usually have pressure to find and retain a job or else they will face an unemployment nightmare. I give the deepest sympathy to the homeless specifically during the winter.</p>
+        <p>I concluded that life without rent and without much pressure is a better life.</p>
       </MyReactTooltip>
-      &nbsp;creeped in and I decided to go back to Thailand. Not long before the end of the job, my mother visited me from Norway and I also later&nbsp;
+      &nbsp;creeped in and I decided not to extend the contract. Not long before the end of the job, my mother visited me from Norway and I also later&nbsp;
       <MyReactTooltip id="tt-s-berlinnorwayvisit" text="visited">
-        <p></p>
-        <p></p>
+        <p>She lived in Kristiansan, Norway. The weather at that time was snowy and the town was crowded. We went with her husband on a ferryboat to Denmark where he bought an art piece and we visited an underwater museum.</p>
+        <p>Norway was an interesting place with very high social welfare. At the same time, the fee when people misbehaved was very high. The food was very expensive but the nature there was lovely. Some people caught lobsters themselves and some picked berries in the wild.</p>
       </MyReactTooltip>
-      &nbsp;her back.
+      &nbsp;her back. I had to&nbsp;
+      <MyReactTooltip id="tt-s-schipolsleep" text="sleep">
+        <p>Because of the weather, I got free food from the airline at both airports in Norway and Amsterdam. I was entitled to a free hotel also in Amsterdam but there was no vacancy nearby. So, I slept in the airport at the spot near the heater. To my surprise, many people did the same. It was one of the best sleep ever since I added this to my list of "interesting stuff to tell others".</p>
+      </MyReactTooltip>
+      &nbsp;at Schiphol airport on the way back to Germany.
     </div>
   </>
