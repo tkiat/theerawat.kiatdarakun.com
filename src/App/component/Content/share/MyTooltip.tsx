cf74409880590b90185900a121e90f7631b63c42
Theerawat Kiatdarakun <tkiat@tutanota.com>
2021-11-18 15:52:41 +0700

    feat: replace MyTooltip manual id with uuid package, also add MyTooltipWithDelay component

--- src/App/component/Content/share/MyTooltip.tsx
+++ src/App/component/Content/share/MyTooltip.tsx
@@ -1,23 +1,38 @@
 import React from "react"
-import ReactTooltip from "react-tooltip"
+import ReactTooltip, {TooltipProps} from "react-tooltip"
+import {v4 as uuidv4} from "uuid"
 
-export const MyTooltip = (props: any): React.ReactElement =>
-  <>
-    <span className="tooltip-front" data-tip data-for={props.id}>{props.text}</span>
-    <ReactTooltip
-      className="tooltip"
-      arrowColor="transparent"
-      overridePosition={({left, top}, _e, _t, _) => ({
-        top,
-        left: Math.max(left, 0),
-      })}
-      {...props}
-    >
-      {props.children}
-    </ReactTooltip>
-  </>
+type MyTooltipProps = TooltipProps & {text: string}
 
-export const MyTooltipWithDelay = (props: any): React.ReactElement =>
-  <MyTooltip delayHide={500} delayUpdate={500} {...props}>
-    {props.children}
+export const MyTooltip = (p: MyTooltipProps): React.ReactElement => {
+  const id = uuidv4()
+  return (
+    <>
+      <span className="tooltip-front" data-tip data-for={id}>{p.text}</span>
+      <ReactTooltip
+        className="tooltip"
+        arrowColor="transparent"
+        overridePosition={({left, top}, _e, _t, _) => ({
+          top,
+          left: Math.max(left, 0),
+        })}
+        id={id}
+        {...p}
+      >
+        {p.children}
+      </ReactTooltip>
+    </>
+  )
+}
+
+export const MyTooltipWithDelay = (p: MyTooltipProps): React.ReactElement =>
+  <MyTooltip
+    className="tooltip tooltip--withdelay"
+    delayHide={500}
+    delayShow={500}
+    delayUpdate={500}
+    effect="solid"
+    {...p}
+  >
+    {p.children}
   </MyTooltip>
