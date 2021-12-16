395150e4240a4fca56b1e17928ba9b3ab00f30ef
tkiat <tkiat@tutanota.com>
2021-12-16 18:30:20 +0700

    UPDATE

--- src/App/component/Content/page/Consume/Course.tsx
+++ src/App/component/Content/page/Consume/Course.tsx
@@ -9,19 +9,22 @@ export const Course = (): React.ReactElement =>
       <li>
         <span className="bold highlight">2020: </span>
         <MyTooltipWithDelay text="Introduction to Personal Branding">
-          This coursera <a href="https://www.coursera.org/learn/personal-branding">course</a> is about how to construct personal branding. I recommend this course because a good personal branding can be life-changing for some people. I got the <a href="https://coursera.org/verify/AZD6S5JVYEG8">verified certificate</a> for free.
           <p>
+            I was going to build my portfolio site and gaining more perspective about how to put myself out there sounds like a good idea. I, by chance, discovered <a href="https://www.coursera.org/learn/personal-branding">this course</a> and I got the <a href="https://coursera.org/verify/AZD6S5JVYEG8">verified certificate</a> for free because of the coronavirus outbreak giveaway.
+          </p>
+          <p>
+            This coursera is about how to construct personal branding. The course has no practical assignments, just some talks. The instructor talks very casually and relaxing. I would say this is more like a fun short course around 4-10 hours which is not at all in-depth. The course is worth it to me though as I haven't been aware of some of the tips before.
           </p>
         </MyTooltipWithDelay>
 
         &nbsp;|&nbsp;
         <MyTooltipWithDelay text="Sit less, get active">
-          I got the <a href="https://coursera.org/share/896aa29cfb15efd3a8f0d08de9134c99">verified certificate</a> for free and I sat all day so I took it. I forgot all the thing except the course title itself which should be enough. This <a href="https://www.coursera.org/learn/get-active">course</a> convinces the students to excercise more.
+          I got the free <a href="https://coursera.org/share/896aa29cfb15efd3a8f0d08de9134c99">verified certificate</a> offer and I sat all day so I took it. I forgot all the thing except the course title itself which should be enough. This <a href="https://www.coursera.org/learn/get-active">course</a> convinces the students to excercise more.
         </MyTooltipWithDelay>
 
         &nbsp;|&nbsp;
         <MyTooltipWithDelay text="Cloud Computing Basics">
-          I got the <a href="https://coursera.org/share/7fa417bc8244a27c8ccb08295906fb99">verified certificate</a> for free so I took it. This coursera <a href="https://www.coursera.org/learn/cloud-computing-basics">course</a> is too basic. You are better off reading Wikipedia in my opinion.
+          I took this short [course](https://www.coursera.org/learn/cloud-computing-basics) for fun. It is a guide to cloud computing with a brief explanation of related technologies. This is a very short course (probably < 3 hours with some preknowledge). The instructor at times reads what is shown on slides and not explains everything that appears on them so I personally don't recommend this course. You are better off reading Wikipedia in my opinion. At the end, I got the <a href="https://coursera.org/share/7fa417bc8244a27c8ccb08295906fb99">verified certificate</a> for free.
         </MyTooltipWithDelay>
       </li>
       <li>
@@ -36,6 +39,12 @@ export const Course = (): React.ReactElement =>
           <p>
             I was addicted to the previous alogorithm specialization so I took another one. This coursera <a href="https://www.coursera.org/specializations/data-structures-algorithms">specialization</a> contains 6 courses. The hidden corner cases for the assignments alone made this course already worth it. I paid 49 USD for the <a href="https://coursera.org/verify/specialization/MHRFWABPYTJU">verified certificate</a>.
           </p>
+          <p>
+            The specialization offers a lot of coding assignments with a variety of programming languages to choose from. The runtime limit for each submission depends on the programming languages where C++ appeared to be less forgiving. So I wrote everything in C++ :) (except one capstone question where I had to use Python instead). This is the hardest MOOC course by far (to me) so non full-time learners might take longer than a month. One question in the capstone project took me a day!
+          </p>
+          <p>
+            Content-wise, the specialization (6 courses) is about basic data structure and algorithms from basic to more advanced. The genome-related courses provide me the insight into how the algorithm can help improve real-life applications. It is hard for me but there is a healthy amount of hint from the discussion forum. Code copy/paste activity is strongly discouraged of course, but pseudocodes are available. I would recommend this course to anyone.
+          </p>
         </MyTooltipWithDelay>
 
         &nbsp;|&nbsp;
@@ -44,6 +53,7 @@ export const Course = (): React.ReactElement =>
             I was unexplainably drawn to computer science just a month after 4.5 years in Germany. Algorithms seemed like the most important basics so I took it. This coursera <a href="https://www.coursera.org/specializations/algorithms">specialization</a> contains 4 algorithm-related courses focusing on the theory. I paid 49 USD for the <a href="https://coursera.org/verify/specialization/LP2AXJ7TUNRV">verified certificate</a>.
           </p>
           <p>
+            The specialization focuses deeply on theory, so deep that I skipped some lectures! I would recommend this course for an aspiring computer scientist. By the way, I am not impressed with the assignment format. You can just put a correct number to pass the assignment.
           </p>
         </MyTooltipWithDelay>
       </li>
