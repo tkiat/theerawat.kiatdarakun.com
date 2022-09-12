74ac7baccd72fc11cc9976dff1ed4cf4d11c186d
tkiat <tkiat@tutanota.com>
2022-09-12 18:07:30 +0700

    UPDATE

--- src/App/component/Content/page/Hi.tsx
+++ src/App/component/Content/page/Hi.tsx
@@ -2,20 +2,10 @@ import React from 'react'
 
 export const Hi = (): React.ReactElement => (
   <div className="page-hi">
-    <h1>
-      <span role="img" aria-hidden="true">{'\u{1F600}'}</span>
-      Hi! I am Theerawat.
-    </h1>
-    <p>
-      I have an engineering background who become interested in simple living with less impact. My hobby usually involves Web, coding, and Linux because they enables the freedom in presentation and computing.
-    </p>
-    <p>And transparency as well.</p>
-    <p>
-      I would rate my English skill as <i>Full Professional proficiency</i>. I communicate with non-Thais at ease while working in Germany. I like to read.
-    </p>
-    <p className="sleep">
-      I also love to sleep.{' '}
-      <span role="img" aria-hidden="true">{'\u{1F634}'}</span>
-    </p>
+    <p><span className="hi-highlight big-text">Hi! I am Theerawat <span role="img" aria-hidden="true">{'\u{1F600}'}</span></span>. I found the wisest way to live by the way of philosophy. Such way of life is to live by the reality (i.e., the awareness of everything), for it is the only thing that is not illusory (i.e., not created out of thin air).</p>
+
+    <p>My life purpose is to live by the awareness and, if time and resources allowed, help others to do the same as well. With the analytical mindset, I dissect my actions to determine if they align with the reality. It is not surprising that I follow minimalist and low impact lifestyles.</p>
+
+    <p>I have an engineering background in telecommunications who can communicate in English at ease in the workplace. My hobbies include (web) programming and Linux because they enable the freedom in presentation and computing. <span className="hi-highlight" role="img" aria-hidden="true">{'\u{1F60E}'}</span></p>
   </div>
 )
