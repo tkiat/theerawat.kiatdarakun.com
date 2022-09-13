67cd8a3ca2f6b0e9307c5e70b66a952a0a5a12ca
tkiat <tkiat@tutanota.com>
2022-09-13 17:36:53 +0700

    UPDATE

--- src/App/component/Content/page/Story/06-LongUnemployment.tsx
+++ src/App/component/Content/page/Story/06-LongUnemployment.tsx
@@ -4,9 +4,10 @@ import {MyTooltip} from "../../share/MyTooltip"
 
 export const LongUnemployment = (): React.ReactElement =>
   <>
-    <h1>28 Years - 32 Years Old (Voluntary Unemployment)</h1>
+    <h1>28 - 32 Years Old (Voluntary Unemployment)</h1>
 
-    <p>I became utterly confused after coming back to Thailand.&nbsp;
+    <p>
+      I became utterly confused after coming back to Thailand.&nbsp;
       <MyTooltip text="Many things">
         The sky train near my house was finished. A new shopping mall emerged.
       </MyTooltip>
@@ -14,7 +15,7 @@ export const LongUnemployment = (): React.ReactElement =>
       <MyTooltip text="nothing">
         People still enjoy and suffer, and they still do stuff they think they need to do. Many things that I didn't consider useful anymore still engrossed so many people.
       </MyTooltip>
-      &nbsp;was really changed. After a month, I applied to only three jobs but didn't pass all the interviews so I stopped my job search. This failure, coupled with the fact that I just bought a new IKEA table and a new 27-inch monitor, makes video games my new&nbsp;
+      &nbsp;was really changed. After a month, I applied to only three jobs but didn't pass all the interviews so I stopped my job search. This failure, coupled with the fact that I just bought a new IKEA table and a new 27-inch monitor, made video games my new&nbsp;
       <MyTooltip text="full-time job">
          I was then a mere consumer who didn't produce anything.
       </MyTooltip>
@@ -34,7 +35,7 @@ export const LongUnemployment = (): React.ReactElement =>
       <MyTooltip text="Thinkpad X200">
         They allowed me to install open-source WiFi firmware and bootloader (Libreboot then coreboot). The proprietary counterparts might contain spyware because no one can see the code.
       </MyTooltip>
-      . I was much more interested in thinkering with software than playing video games, so I stopped playing games (intensively) ever since.
+      . I was much more interested in tinkering with software than playing video games, so I stopped playing games (intensively) ever since.
     </p>
 
     <p>
@@ -42,8 +43,18 @@ export const LongUnemployment = (): React.ReactElement =>
       <MyTooltip text="lean and liberating">
         I replaced my iPad with a cheaper tablet and quitted buying frozen food and chips from the convenience store.
       </MyTooltip>
-      &nbsp;than something convenient and costly. After reading a few philosophy blog posts and books,
+      &nbsp;than something convenient and costly. I began to perceive sufferings of everything around me. My perspective was broaden, and I knew I wanted “freedom”. I immersed myself in (web) programming and Linux, thinking that they will give me freedom I want. My desire for freedom extends to other beings as well. I has a thought of using less waste and going vegan, though I still thought of veganism as “too much”.
+    </p>
 
+    <p>This eventually led to the strong urge to discover the truth of the universe. I decided to discover the “why?” to live before searching for a job. For almost a year, I read some philosophy blog posts and also introspected a lot about the fundamental nature of everything. Why should I eat or survive? Does a tiger have right to kill other animals? What is good and bad? Do we have objective life purposes? Does God exist? Does rebirth exist?</p>
 
-      Now, I believe I am much wiser than I ever was before. I appreciate the given opportunity to introspect and I become more aware of my values. I now have a clearer purpose to pursue.</p>
+    <p>I asked all these questions to discover why (and how) I should live. I finally came to the conclusion that the life purpose can be anything (fame, nirvana, heaven, a lot of sensual pleasure, etc.) since goodness and badness don't inhere the universe. However, it is the wisest to use the awareness to guide my life, for it is the only thing that I don't create out of thin air. I long for the truth, and the wisest way of life is the answer I have been searching for.</p>
+
+    <p>
+      I have used my awareness to observe everything around me, especially the darkness that resides within me. While it can take forever to uncover eveyrthing, I now believe I am much wiser than I ever was before, for I have examined my life greatly. I now know the reasons behind&nbsp;
+      <MyTooltip text="each action">
+      </MyTooltip>
+      &nbsp;
+
+    I appreciate the given opportunity to introspect. I now have a clearer purpose to pursue.</p>
   </>
