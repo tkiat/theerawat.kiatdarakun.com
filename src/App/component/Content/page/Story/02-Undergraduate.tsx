1278c23f9ff1156060b934ae676d0be0a0adac51
tkiat <tkiat@tutanota.com>
2022-09-13 12:02:20 +0700

    UPDATE

--- src/App/component/Content/page/Story/02-Undergraduate.tsx
+++ src/App/component/Content/page/Story/02-Undergraduate.tsx
@@ -6,16 +6,15 @@ export const Undergraduate = (): React.ReactElement =>
   <>
     <h1>19 - 23 Years (Bachelor's Degree)</h1>
 
-    <p>My life changed a lot more after I joined the university. There were ways too many things to do but I still mostly played video games. Again, many people were interested in getting a girlfriend but I didn't seem to care. Things that interested me outside of the study are volunteer camps (usually a few days), some exercises (swimming, jogging, badminton), and traveling (not that far, usually by free trains). Everything new was opened to me and I appreciated that.</p>
+    <p>There were ways too many things to do but I still mostly played video games (mostly DotA). Things that interested me outside of the study are volunteer camps (usually a few days each), some exercises (swimming, jogging, badminton), and traveling (not that far, usually by free trains). Everything new was opened to me and I appreciated that.</p>
 
-    <p>
-      The English curriculum broadened my horizon (in both technical knowledge and language senses). My faculty, at the time, had three choices: electronics, mechatronics, and communications. I preferred communications over the other two so I did an internship at&nbsp;
+    <p>My life changed a lot more after I joined the university. The English curriculum broadened my horizon (in both technical knowledge and language senses). My faculty, at the time, had three choices: electronics, mechatronics, and communications. I preferred communications over the other two so I did an internship at&nbsp;
       <MyTooltip text="Thaicom">
-        It was a three-month internship in 2011 at Ladlumkaew, Thailand. I mainly observed the workers there do the tasks. There was a satellite installation training that I took for free. The final project is about socket programming between a signal generator and a spectrum analyzer. I chose VB.net to carry out the task. Why? Because it seemed easy to use! During the free time, I wrote a Mario game with VB.net as a practice.
+        It was a three-month internship in 2011 at Ladlumkaew, Thailand. I mainly observed the workers there do the tasks. There was a satellite installation training that I took for free. The final project is about socket programming between a signal generator and a spectrum analyzer. I chose VB.net to carry out the task because it seemed easy to use! During free time, I wrote a Mario game with VB.net for fun.
       </MyTooltip>
       &nbsp;and later&nbsp;
       <MyTooltip text="thesis">
-        The thesis was organized into two parts: an abstract in the first semester and the execution in the second semester. I chose the uplink and downlink-related topic and I didn't actually know how to carry it out so I was the only one who changed the topic in the second semester. It was about the entropy of Thai text so I was proud to say that the Tripitaka was part of my thesis!
+        The thesis was organized into two parts: an abstract in the first semester and the execution in the second semester. I chose the uplink and downlink-related topic and I didn't actually know how to carry it out so I was the only one who changed the topic in the second semester. It was about the entropy of Thai text so I was proud to say that the Tripitaka was a part of my thesis!
       </MyTooltip>
       &nbsp;in this area. These experiences later inspired me to begin the master's degree in Communications Engineering in 2013.
     </p>
--- src/App/component/Content/page/Story/03-MasterDegree.tsx
+++ src/App/component/Content/page/Story/03-MasterDegree.tsx
@@ -16,7 +16,8 @@ export const MasterDegree = (): React.ReactElement =>
       </MyTooltip>
       . The application for RWTH Aachen required a bit of&nbsp;
       <MyTooltip text="preparation">
-        I needed to send my IELTS scores and also open letters of recommendation. The annoying part was that they also require me to take GRE test (which cost around 180 USD). I was so lazy that I took only one day to prepare it. The score was not nice except for the qualitative section. I had to send applications via mail also.
+ I had to send applications via mail also.
+        I needed to send my IELTS scores and also open letters of recommendation. The annoying parts were that I had to send the application via (non-electronics) mail and take GRE test (which cost around 180 USD). I was so lazy that I took only one day to prepare it. Needless to say, the scores, except the familiar qualitative section, were not nice.
       </MyTooltip>
       &nbsp;but I did it anyway.
     </div>
@@ -24,12 +25,11 @@ export const MasterDegree = (): React.ReactElement =>
     <div>
       On the first day of arrival, I had the appointment with&nbsp;
       <MyTooltip text="two kind Thai students">
-        One met me at Cologne and one took a taxi with me from Aachen Hofbahnhof (train station) to my room. I just met an unknown German lady (probably around 50 years old) who stood with me at Hofbahnhof because she said it was dangerous (not true in my opinion).
+        One met me at Cologne and one took a taxi with me from Aachen Hofbahnhof (train station) to my room (which I paid the deposit and the rent in advance). I just met an unknown German lady (probably around 50 years old) who stood with me at Hofbahnhof because she said it was dangerous (not true in my opinion).
       </MyTooltip>
       &nbsp;who will take me to my single-room apartment. I adapted quickly to a new&nbsp;
       <MyTooltip text="lifestyle">
-        <p>I had to cook myself. I always bought pre-made food in Thailand but it was prohibitive to buy them in Germany than cooking it myself (it's like 1.5 EUR vs 5 EUR).</p>
-        <p>The climate was quite the opposite of Thailand where I had to heat my room instead of cooling it. My cloth needed to be thick during the cold winter when I could sometimes expect snow.</p>
+        I had to cook myself. I always bought pre-made food in Thailand but it was more prohibitive to buy them in Germany than cooking it myself (it's like 1.5 EUR vs 5 EUR). The climate was quite the opposite of Thailand where I had to heat my room instead of cooling it. My cloth needed to be thick during the cold winter when I could sometimes expect snow.
       </MyTooltip>
       , went to&nbsp;
       <MyTooltip text="Dreilaenderpunkt">
@@ -37,12 +37,11 @@ export const MasterDegree = (): React.ReactElement =>
       </MyTooltip>
       , and finally joined the&nbsp;
       <MyTooltip text="orientation">
-        This was when I realized more than half was Indian. A very little Chinese was here in my batch which was a little surprising. There was only one German. If you want to meet Germans in a class, take a class in German, not in English.
+        This was when I realized more than half was Indian. A very little Chinese was here in my batch which was a little surprising. There was only one German. If you want to meet more Germans in a class, you probably want to take a class in German, not in English.
       </MyTooltip>
       . The&nbsp;
       <MyTooltip text="first semester">
-        <p>I took all the courses as many as possible because I didn't have anything better to do. The exam here was taken only one time at the end of the semester and very short (15-minute oral exam or 1.5 hours written exam).</p>
-        <p>In my opinion, if you believe you cannot process the exam quickly, don't come. You will be rejected from the university if you fail the exam of the same course three times in a row.</p>
+        I took all the courses as many as possible because I didn't have anything better to do. The exam here was taken only one time at the end of the semester and very short (15-minute oral exam or 1.5 hours written exam). In my opinion, if you believe you cannot process the exam quickly, don't come. You will be rejected from the university if you fail the exam of the same course three times in a row.
       </MyTooltip>
       &nbsp;passed quickly. The second semester was really interesting to me as I will share the same&nbsp;
       <MyTooltip text="room">
@@ -52,15 +51,17 @@ export const MasterDegree = (): React.ReactElement =>
       <MyTooltip text="gym membership">
         <p>The membership required participants to enroll in the introductory course first which lasted a few days and cost me 30 EUR. I, along with the other 5 German students, were introduced to how to use each machine but the trainer had to speak to me in English. The bathroom was full of naked people but I used a separate room myself because I didn't want to hurt their ego. Smaller is better.</p>
       </MyTooltip>
-      &nbsp;and travel with my&nbsp;
+      &nbsp;and travelled with my&nbsp;
       <MyTooltip text="bicycle">
-        <p>I won the touring bike from the auction on eBay for 140 EUR. It was cheap given the quality: too many gears, water bottle holder, two seats, and 28-inch wheel size. I took the free train to Duesseldorf where I met the seller. In Germany, you have to pay for a ticket for a bicycle if you carry it on a train. Make sense!</p>
-        <p>I was about to get punished by the train officer that day but luckily a German guy talked with the office and somehow I had to pay only 6 EUR bicycle fee, not an additional, probably 40 EUR, punishment fee.</p>
+        <p>I won the touring bike from the auction on eBay for 140 EUR. It was cheap given the quality: many gears, water bottle holder, two seats, and 28-inch wheel size. I took the free train to Duesseldorf where I met the seller. In Germany, you have to pay for a ticket for a bicycle if you carry it on a train. Make sense!</p>
+
+        <p>I was about to get punished by the train officer that day but luckily a German guy talked with the office and somehow I had to pay only 6 EUR bicycle fee, not an additional, probably around 40 EUR, punishment fee. I was fined 3 times with my bicycle usage. The first time was for the absence of rear light, and the other twos were too much speed.</p>
       </MyTooltip>
       &nbsp;
       <MyTooltip text="nearby">
         <p>Maastricht is a beautiful city in the Netherlands. Shopping malls and the river port attracted me a lot. They were usually full of people and I went there more than 15 times! The best part was the quality of the bicycle road to the city. It took me 1.5 hours to reach this 35km destination. There was an American war memorial on the way to the city.</p>
-        <p>Monschau is a very beautiful town with some small rivers and a castle. Bar the constant flux of visitors, it was very nice to live there. It took me almost 2 hours by bike because of the slope but I spent only 1 hour on the way back. I went there two times by bike, alone.</p>
+
+        <p>Monschau is a very beautiful town with some small rivers and a castle. Barred the constant flux of visitors, it was very nice to live there. It took me almost 2 hours by bike because of the slope but I spent only 1 hour on the way back. I went there two times by bike, alone.</p>
       </MyTooltip>
       . In general, you should expect to spend a little more than&nbsp;
       <MyTooltip text="two years">
@@ -72,8 +73,8 @@ export const MasterDegree = (): React.ReactElement =>
     <div>
       I decided to do the thesis before the internship because I found this an easier path. I couldn't find any other interesting thesis so I did the practical test&nbsp;
       <MyTooltip text="stuff">
-        <p>It was a test on FPGA boards about the coding scheme that my supervisor researched. He wanted the practical test to confirm his theory. I learn about how to hack the board and carry out the test methodologically.</p>
+        <p>It was a test on FPGA boards about the coding scheme that my supervisor were conducting research on. He wanted the practical test to confirm his theory. I learn about how to hack the board and carry out the test methodologically. I had to modify the existing C and Verilog codes to conduct the test.</p>
       </MyTooltip>
-      &nbsp;which should help me find the corporate internship as well.
+      , and I expect it to help me find the corporate internship.
     </div>
   </>
