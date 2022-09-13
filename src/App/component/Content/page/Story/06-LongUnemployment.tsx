481102cfdf216642d9d7ed9b767c10d85c13e8e4
tkiat <tkiat@tutanota.com>
2022-09-13 20:40:57 +0700

    UPDATE

--- src/App/component/Content/page/Story/06-LongUnemployment.tsx
+++ src/App/component/Content/page/Story/06-LongUnemployment.tsx
@@ -9,17 +9,17 @@ export const LongUnemployment = (): React.ReactElement =>
     <p>
       I became utterly confused after coming back to Thailand.&nbsp;
       <MyTooltip text="Many things">
-        The sky train near my house was finished. A new shopping mall emerged.
+        The sky train near my house was finished; a new shopping mall emerged.
       </MyTooltip>
       &nbsp;seemed to change, yet&nbsp;
       <MyTooltip text="nothing">
         People still enjoy and suffer, and they still do stuff they think they need to do. Many things that I didn't consider useful anymore still engrossed so many people.
       </MyTooltip>
-      &nbsp;was really changed. After a month, I applied to only three jobs but didn't pass all the interviews so I stopped my job search. This failure, coupled with the fact that I just bought a new IKEA table and a new 27-inch monitor, made video games my new&nbsp;
+      &nbsp;was really changed. After a month, I applied to only three jobs but didn't pass all the interviews so I stopped my job search and then played video games like a&nbsp;
       <MyTooltip text="full-time job">
-         I was then a mere consumer who didn't produce anything.
+        I bought a new IKEA table and a new 27-inch monitor, making it easier to engross myself in video games. I was then a mere consumer who didn't produce anything.
       </MyTooltip>
-      &nbsp;for more than a year. One day, I noticed that my self-assembled PC was actually&nbsp;
+      &nbsp;for more than a year. One day, I noticed that my self-assembled PC was&nbsp;
       <MyTooltip text="problematic">
         It incurred much electricity cost and was also prone to the occasional power shortage.
       </MyTooltip>
@@ -31,7 +31,7 @@ export const LongUnemployment = (): React.ReactElement =>
     </p>
 
     <p>
-      I first replaced Microsoft Vista that comes with it with Ubuntu and then Void Linux. The Void Linux was performant and that was an eye-opener for me. I became interested in this liberating piece of software (Linux) and abandoned my PC. I sold my PC afterward and bought two used&nbsp;
+      I first replaced Microsoft Vista that comes with it with Void Linux. It was performant and that was an eye-opener for me. I became interested in this liberating piece of software (Linux), and I sold my PC afterward and bought two used&nbsp;
       <MyTooltip text="Thinkpad X200">
         They allowed me to install open-source WiFi firmware and bootloader (Libreboot then coreboot). The proprietary counterparts might contain spyware because no one can see the code.
       </MyTooltip>
@@ -43,18 +43,33 @@ export const LongUnemployment = (): React.ReactElement =>
       <MyTooltip text="lean and liberating">
         I replaced my iPad with a cheaper tablet and quitted buying frozen food and chips from the convenience store.
       </MyTooltip>
-      &nbsp;than something convenient and costly. I began to perceive sufferings of everything around me. My perspective was broaden, and I knew I wanted “freedom”. I immersed myself in (web) programming and Linux, thinking that they will give me freedom I want. My desire for freedom extends to other beings as well. I has a thought of using less waste and going vegan, though I still thought of veganism as “too much”.
+      &nbsp;than something convenient and costly. My perspective was broaden, and I knew I wanted “freedom”. I immersed myself in (web) programming and Linux, thinking that they will give me freedom I want. My desire for freedom extends to other beings as well. This was when I began to perceive sufferings of everything around me. I has a thought of using less waste and going vegan, though I still thought of veganism as “too much”.
     </p>
 
-    <p>This eventually led to the strong urge to discover the truth of the universe. I decided to discover the “why?” to live before searching for a job. For almost a year, I read some philosophy blog posts and also introspected a lot about the fundamental nature of everything. Why should I eat or survive? Does a tiger have right to kill other animals? What is good and bad? Do we have objective life purposes? Does God exist? Does rebirth exist?</p>
-
-    <p>I asked all these questions to discover why (and how) I should live. I finally came to the conclusion that the life purpose can be anything (fame, nirvana, heaven, a lot of sensual pleasure, etc.) since goodness and badness don't inhere the universe. However, it is the wisest to use the awareness to guide my life, for it is the only thing that I don't create out of thin air. I long for the truth, and the wisest way of life is the answer I have been searching for.</p>
+    <p>
+      This eventually led to the strong urge to discover why (and how) I should live before beginning my job search again. For almost a year, I read some philosophy and also&nbsp;
+      <MyTooltip text="introspected a lot">
+        Why should I eat or survive? Does a tiger have right to kill other animals? What is good and bad? Do we have objective life purposes? Does God exist? Does rebirth exist?
+      </MyTooltip>
+      &nbsp;about the fundamental nature of things. I finally came to the conclusion that life purpose can be&nbsp;
+      <MyTooltip text="anything">
+        Since goodness and badness don't inhere the universe and humans have capable faculties to set anything as a goal, e.g., fame, nirvana, heaven, sensual pleasure, life purpose can be anything.
+      </MyTooltip>
+      . However, it is the&nbsp;
+      <MyTooltip text="wisest">
+        Being wise is having the ability to make good judgments. Since the judgment is better with more evidence, it is the wisest to make a judgement based on what actually exists, not something created out of thin air. In other words, it is the wisest to use the awareness to guide my life.
+      </MyTooltip>
+      &nbsp;to use the awareness to guide my life. I long for the truth, and this is the answer I have been searching for.</p>
 
     <p>
-      I have used my awareness to observe everything around me, especially the darkness that resides within me. While it can take forever to uncover eveyrthing, I now believe I am much wiser than I ever was before, for I have examined my life greatly. I now know the reasons behind&nbsp;
-      <MyTooltip text="each action">
+      While it can take forever to uncover everything using my awareness, I now believe I am much wiser than I ever was before, for I have examined my life including the darkness that resides within me. I now know the reasons behind&nbsp;
+      <MyTooltip text="many actions">
+        For example, I should turn on a fan when my body needs it, and one sign for this is sweat.
       </MyTooltip>
-      &nbsp;
+      &nbsp;I appreciate the given opportunity to introspect. I now know why and how to live.
+    </p>
 
-    I appreciate the given opportunity to introspect. I now have a clearer purpose to pursue.</p>
+    <p>Why should I live? Using my awareness, I conclude that my body wants to survive (as it heals itself), and the same applies to other beings as well. Since I need to kill other beings for food, both decisions (to commit suicide and continue to hurt others) neither fully respect nor disrespect the reality. The only way to prevent this dilemma is to prevent myself from being born in the first place, which is impossible. Therefore, I must choose one of these two valid choices.</p>
+
+    <p>Of course, I decided to live. </p>
   </>
