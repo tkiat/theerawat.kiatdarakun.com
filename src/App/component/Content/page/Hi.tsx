c1b5f6802090def877e2ec1ecfca772dd6beeaab
tkiat <tkiat@tutanota.com>
2022-11-11 17:01:39 +0700

    UPDATE

--- src/App/component/Content/page/Hi.tsx
+++ src/App/component/Content/page/Hi.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export const Hi = (): React.ReactElement => (
   <div className="page-hi">
-    <p><span className="hi-highlight big-text">Hi! I am Theerawat <span role="img" aria-hidden="true">{'\u{1F600}'}</span></span>. I regard the wisest way of life as living by awareness since it is the only thing that is not illusory (i.e., not created out of thin air). My life purpose is to live my life that way and, if time and resources allowed, help others to do the same as well.</p>
+    <p>Almost every quality has some disadvantages. Being very rich is subject to being exploited; being too white is subject to being disliked by anti-white people; being a businessman is subject to a businessman's problems; being a carpenter is subject to a carpenter's problems. The exception is being wise: better judgment is always desirable.</p>
 
-    <p>With the analytical mindset, I dissect my actions to determine if they align with the reality. It is not surprising that I follow minimalist and low impact lifestyles.</p>
+    <p>Therefore, I, <span className="hi-highlight big-text">Theerawat <span role="img" aria-hidden="true">{'\u{1F600}'}</span></span>, regard the wisest way of life as the most desirable aspiration. Such life is based on awareness, memory, and logic since the best judgment is not based on illusory matters. My life purpose is to live that way and, if resources allowed, help others to do the same as well. For now, I'd like to find a useful job first.</p>
 
-    <p>I have an engineering background in telecommunications who can communicate in English at ease in the workplace. My hobbies include (web) programming and Linux because they enable the freedom of presentation and computing. <span className="hi-highlight" role="img" aria-hidden="true">{'\u{1F60E}'}</span></p>
+    <p>With an analytical mindset, I dissect my actions to determine if they align with reality. It is not surprising that I follow minimalist and low-impact lifestyles. I have an engineering background in telecommunications and can communicate in English at ease. My hobbies include (web) programming, Linux, and reading <span className="hi-highlight" role="img" aria-hidden="true">{'\u{1F60E}'}</span>.</p>
   </div>
 )
