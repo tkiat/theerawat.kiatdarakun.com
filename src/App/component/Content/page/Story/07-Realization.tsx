e54c9f0ece51ea20f7dd05d2777a26d63fb18875
tkiat <tkiat@tutanota.com>
2023-01-30 12:51:12 +0700

    UPDATE

---
+++ src/App/component/Content/page/Story/07-Realization.tsx
@@ -0,0 +1,35 @@
+import React from "react"
+
+import {TooltipText} from "../../share/Tooltip"
+
+export const Realization = (): React.ReactElement =>
+  <>
+    <p>
+      <b className="big highlight">32 Years Old (Realization)</b> — After a while, I realized that awareness alone&nbsp;
+      <TooltipText text="cannot always dictate">
+        Sure, it tells me to change my position when I ache, but what about eating? I currently need to harm other sentient beings for food (even vegan food due to insects), which is a conflict since my awareness tells me that all beings want to survive (as it, e.g., heals itself). Should I commit suicide or continue living?
+      </TooltipText>
+      &nbsp;what I should do. After days of confusion, I got an epiphany that it is the wisest not to seek truths but instead ask for “what truly matter?” and live by it because of this&nbsp;
+      <TooltipText text="reason">
+        TODO
+        I am a sentient being so every of my action and thought is though subjective lenses.
+      </TooltipText>
+      . After the&nbsp;
+      <TooltipText text="first">
+        Everything that truly matters lies within sentient beings. Why? Without sentient beings, who's gonna complain after Saturn crashes with a moon right?
+      </TooltipText>
+      ,&nbsp;
+      <TooltipText text="second">
+        Everything that truly matters is limited to existing qualities or flows inside sentient beings. Why? “truly” in “truly matters” means something that is not made-up. For example, I would think a computer that I are seeing is “true”, unlike a computer in a fairy tale. Anything that is not made-up can only be discovered through six senses, not anything in the mind. Though, I can use memory and denotation, for example, to refer to past perceptions. They are still truths, for they don't change anything.
+      </TooltipText>
+      , then&nbsp;
+      <TooltipText text="third">
+        Everything that truly matters is limited to existing flows, not qualities, inside sentient beings. Why? Existing qualities within a sentient being themselves (like hands, white skin, etc.) are just mere states and cannot be reversed, and thus they cannot matter. On the other hand, existing flows can be reversed, and thus they can “matter”.
+      </TooltipText>
+      &nbsp;considerations, I concluded that <strong>the only things that <i>can</i> truly matter, to any sentient being, lie within the flows inherent in all sentient beings</strong>. This led to my realization that the elimination of ego, greed, anger, ignorance, etc., doesn't truly matter, only such flows truly matter. I became free from all dogmas, beliefs, religions (including Buddhism), and so on.
+    </p>
+
+    <p>
+      I shall devote my life to live by and create works upon what truly matters for the benefits of beings. With this realization, I regard the flows inside me (e.g., my bodily needs) as “matter” so I satisfy them. In the case of a conflict (of flows inside two or more sentient beings) such as eating that I only have two choices: commit suicide or harm some beings to continue living, without any bias, I cannot come to the definitive conclusion. I shall leave two possibilities open and make a decision only if I must. The fact that you see this sentence shows that I decided to continue living.
+    </p>
+  </>
