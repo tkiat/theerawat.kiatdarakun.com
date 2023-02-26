3d8d07526e4a3b67dc3b6afdb9a1ee55ee6e2afa
tkiat <tkiat@tutanota.com>
2023-02-26 14:38:20 +0700

    UPDATE

--- src/App/component/Content/page/Character/Consumables.tsx
+++ src/App/component/Content/page/Character/Consumables.tsx
@@ -61,31 +61,95 @@ export const Consumables = (): React.ReactElement => {
 
   return (
     <section>
-      <h2>Consumables Record</h2>
+      <h2>Consumables</h2>
 
-      <div className="consumables-panel">
-        <Why />&ensp;
-        <How /> —&nbsp;
-        <label className="consumables-panel__label" htmlFor="consumables-panel-select">
-          {isNaN(Number(cur)) ? "Single Week" : "Weekly Avg." }
-        </label>
-        <Select
-          cur={cur}
-          setCur={setCur}
-          avgSummaries={avgSummaries}
-          weeks={weeks}
-        />
-        <Checkboxes fields={fields} setFields={setFields} />
-        <hr />
-      </div>
+      <section>
+        <h3 className="highlight">Record</h3>
 
-      <div className="consumables-display-container">
-        {
-          isNaN(Number(cur)) ?
-            <WeekTable cur={cur} fields={fields} weeks={weeks} />
-          : <AvgChart cur={cur} fields={fields} avgSummaries={avgSummaries} />
-        }
-      </div>
+        <div className="consumables-panel">
+          <Why />&ensp;
+          <How /> —&nbsp;
+          <label className="consumables-panel__label" htmlFor="consumables-panel-select">
+            {isNaN(Number(cur)) ? "Single Week" : "Weekly Avg." }
+          </label>
+          <Select
+            cur={cur}
+            setCur={setCur}
+            avgSummaries={avgSummaries}
+            weeks={weeks}
+          />
+          <Checkboxes fields={fields} setFields={setFields} />
+          <hr />
+        </div>
+
+        <div className="consumables-display-container">
+          {
+            isNaN(Number(cur)) ?
+              <WeekTable cur={cur} fields={fields} weeks={weeks} />
+            : <AvgChart cur={cur} fields={fields} avgSummaries={avgSummaries} />
+          }
+        </div>
+      </section>
+
+      <section>
+        <h3 className="highlight">Cooking</h3>
+
+        <h4>Food</h4>
+
+        <ul className="more-gap-list">
+          <li>
+            <b className="highlight">Meal</b>:&nbsp;
+
+            TODO
+          </li>
+        </ul>
+
+        <h4>Drinks</h4>
+
+        <ul className="more-gap-list">
+          <li>
+            <b className="highlight">Milk</b>:&nbsp;
+
+            <TooltipText text="Soy Milk">
+              I usually use soy milk for my smoothie — <span className="highlight">Recipe</span>: 300-400g soybeans (preferably non-GMO due to excessive glyphosate) and 2.5L water — <span className="highlight">Steps</span>: Wash soybeans, use blender and filter to get the milk, then use medium heat until some bubbles appear, and finally simmer it for 15 minutes.
+            </TooltipText>
+          </li>
+
+          <li>
+            <b className="highlight">Boiled Herbal Teas</b>:&nbsp;
+
+            <TooltipText text="Hibiscus">
+              Unlike many other herbal teas, I find sugar in hibiscus tea especially tasty as it fixes sourness in the tea — <span className="highlight">Recipe</span>: 20 dried hibiscus leaves, 2.5L water, (optional but recommended) 50g brown/crystalline sugar — <span className="highlight">Steps</span>: Wash the leaves and then put it in the water, boil it until it reaches the boiling point then simmer it for 20 minutes, done.
+            </TooltipText>
+            ,&nbsp;
+            <TooltipText text="Lemongrass and Pandan">
+              <span className="highlight">Recipe</span>: 50g dried lemongrass and pandan leaves, 2.5L water — <span className="highlight">Steps</span>: Wash the leaves and then put it in the boiling water, simmer it for 20 minutes, done.
+            </TooltipText>
+            ,&nbsp;
+            <TooltipText text="Bael Fruit">
+              <span className="highlight">Recipe</span>: 80g dried bael fruit, 2.5L water — <span className="highlight">Steps</span>: Wash the fruits and put it in the water, boil it until it reaches the boiling point then simmer it for 20 minutes, done.
+            </TooltipText>
+            ,&nbsp;
+            <TooltipText text="Butterfly Pea Flower">
+              It's best to consume 2-3 pieces a day (as per doctor recommendation). Since my cup is 350ml, I am going to use 20 pieces per 2.5L water for a 7-day serving — <span className="highlight">Recipe</span>: 20 dried leaves, 2.5L water, 1 Tsp lemon (optional, to make it purple) — <span className="highlight">Steps</span>: Wash the leaves and put it in the boiling water for 10 minutes, then stop boiling water and put limeade into it, done.
+            </TooltipText>
+            ,&nbsp;
+            <TooltipText text="Chrysanthemum">
+              <span className="highlight">Recipe</span>: 20 dried chrysanthemum leaves, 2-3 pieces gardenia fruit (optional), 2-3 pieces licorice (optional), 2.5L water — <span className="highlight">Steps</span>: Wash all leaves, rince the chrysanthemum leaves using boiled water for 10 seconds then followed by cool water (optional), then stop the boiling water then steep everything for five minutes, done.
+            </TooltipText>
+          </li>
+
+          <li>
+            <b className="highlight">Teas</b>:&nbsp;
+
+            <TooltipText text="Thai Assam Green">
+              <span className="highlight">Recipe</span>: 6g loose leaves, 350ml water — <span className="highlight">Steps</span>: Rinse the leaves quickly one or two time and then steep it in 90&#8451; water for a minute (first time) and 2 minutes (second time).
+            </TooltipText>
+          </li>
+        </ul>
+
+        <p>TODO</p>
+      </section>
     </section>
   )
 }
