39e8ad983fdb6fd05acd9b8b8adb89f0a310f0c3
tkiat <tkiat@tutanota.com>
2023-03-01 22:50:35 +0700

    UPDATE

--- src/App/component/Content/page/Create.tsx
+++ src/App/component/Content/page/Create.tsx
@@ -54,32 +54,30 @@ const WhatMatter = (): React.ReactElement =>
   <>
     <h2>What Can Truly Matter</h2>
 
-    <p>My contemplative journey to find the meaning of life has culminated in the realization that regardless of the actual reality (be it only mental construct, only material objects, both, or none), what can truly matter cannot go beyond the flow inside sentient beings. The next journey is then to find truths about such flows and gain the good judgment about how to apply these truths in my life, others' lives, and the society.</p>
+    <p>My contemplative journey to find the meaning of life has culminated in the realization that regardless of the actual reality (be it only mental construct, only material objects, both, or none), what can truly matter cannot go beyond the flow inside sentient beings. Next, I will find truths about such flows and determine the skillful ways to apply these truths in my life, others' lives, and society. This should enable readers to focus on what can truly matter instead of some illusory pursuits.</p>
 
-    <p>However, my perception is limited, and I don't know all absolute truths about all such flows. The best I can do is to enable readers to make good judgment in life based on what can truly matter and their ideas of truths. Even if these works don't attract many readers, I am going to finish them anyway since they are what can truly matter. To sum up, I am doing the following works.</p>
+    <p>However, my perception is limited, and I don't know all absolute truths about all such flows. The best I can do is to enable readers to make good judgment in life based on what can truly matter and their ideas of truths. Even if these works don't attract many readers, I am going to finish them anyway since nothing else truly matters. To sum up, I am doing the following works.</p>
 
     <ul className="more-gap-list">
       <li>
-        <p><b className="highlight">What can truly matter</b>. I will write about not only what can truly matter (the flow inside sentient beings) but also what cannot truly matter (like illusory minds and purposes).</p>
+        <p><b className="highlight">What can truly matter</b>. I will write about not only what can truly matter (i.e., the flow inside sentient beings) but also what cannot truly matter (like illusory minds and purposes).</p>
       </li>
 
       <li>
-        <p><b className="highlight">On judgment based on what can truly matter and truths</b>. I don't know all absolute truths, and therefore I select a few common scopes of truths that I believe most people take to make it applicable to most readers. This work should enable readers to focus on what can truly matter instead of some illusory pursuits, which will lead to a wise way of life. Below are my selection of the scopes of truths. For each scope, I am going to determine such judgment in both individual and societal levels.</p>
+        <p><b className="highlight">On judgment based on what can truly matter and truths</b>. I don't know all absolute truths, and therefore I select a few common scopes of truths that I believe most people take to make this work applicable to most readers. Below is my selection of the scopes of truths. For each scope, I am going to determine such judgment at both individual and societal levels.</p>
         <ol>
           <li>
-            <p><b className="highlight">Direct senses and widely accepted scientific research</b>. Direct senses are six senses (taste, smell, vision, hearing, touch, and proprioception); however, they don't seem enough to many. We cannot, for example, always use our senses to determine what is healthy. Therefore, I also add widely accepted scientific research as truths. When both truths are in conflict, I let science win because I believe it sympathizes with most people's ideas of truths.</p>
+            <p><b className="highlight">Direct senses and widely accepted scientific research</b>. Direct senses are six senses (taste, smell, vision, hearing, touch, and proprioception); however, they don't seem enough to many. We cannot, for example, always use our senses to determine what is healthy. Therefore, I also add widely accepted scientific research as truths. When both truths conflict, I let science win because I believe it sympathizes with most people's ideas of truths.</p>
           </li>
 
           <li>
-            <p><b className="highlight">Direct senses, widely accepted scientific research, and beliefs</b>. Beliefs are anything beyond direct senses and science. They can be religious beliefs (most notably those of Buddhism, Christianity, Islam, and Hinduism). It probably takes too much time for me to study all of them so I will begin with Buddhism first.</p>
-
-            <p>When these truths are in conflict, I will give the following priorities: beliefs &gt; science &gt; direct senses because I believe that is what most people who hold such beliefs do. What's interesting is that both science and direct senses that are not in conflict with beliefs are shared by all people. Both truths are therefore perfect underlying foundations of world peace.</p>
+            <p><b className="highlight">Direct senses, widely accepted scientific research, and beliefs</b>. Beliefs are anything beyond direct senses and science. They can be religious beliefs (most notably those of Buddhism, Christianity, Islam, and Hinduism). It probably takes too much time for me to study all of them so I will begin with Buddhism first. When these truths conflict, I will give the following priorities: beliefs &gt; science &gt; direct senses because I believe that is what most people who hold such beliefs do.</p>
           </li>
         </ol>
       </li>
 
       <li>
-        <p><b className="highlight">The exemplary record of my life</b>. I decided to live by what can truly matter based on my idea of truths (direct senses and widely accepted scientific research). I have recorded my consumables, my belongings, my routines, etc., and I will explain the underlying reasons behind each choice. This wish has changed my life completely and driven me to stay healthy and active.</p>
+        <p><b className="highlight">The exemplary record of my life</b>. I decided to live by what can truly matter based on my idea of truths (direct senses and widely accepted scientific research). I have recorded my consumables, my belongings, my routines, etc., and I will explain the underlying reasons behind each choice.</p>
       </li>
     </ul>
 
