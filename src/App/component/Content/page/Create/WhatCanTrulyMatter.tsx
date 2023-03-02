e1bd1b57a09deff3bc7afe200f1ce46a9f5dfe21
tkiat <tkiat@tutanota.com>
2023-03-02 23:54:27 +0700

    UPDATE

--- src/App/component/Content/page/Create/WhatCanTrulyMatter.tsx
+++ src/App/component/Content/page/Create/WhatCanTrulyMatter.tsx
@@ -10,20 +10,21 @@ export const WhatCanTrulyMatter = (): React.ReactElement =>
 
     <ul className="more-gap-list">
       <li>
-        <p><b className="highlight">The meaning of life and what can truly matter</b>. There are many perspectives about the meaning of life, but all of them seem baseless. To escape from baselessness is to begin with what can truly matter and determine the meaning of life from there. This work investigates what can truly matter.</p>
-      </li>
+        <p><b className="highlight">The meaning of life and what can truly matter</b>. There are many perspectives about the meaning of life, but all of them seem baseless. To escape from this pitfall is to begin with what can truly matter and determine the meaning of life from there. This work investigates what can truly matter.</p>
 
-      <li>
-        <p><b className="highlight">On judgment based on what can truly matter and truths</b>. I don't know all absolute truths, and therefore I select a few common scopes of truths that I believe most people take to make this work applicable to most readers. Below is my selection of the scopes of truths. For each scope, I am going to determine such judgment at both individual and societal levels.</p>
+        <p>The next step is to build upon what can truly matter, once realized, to find the meaning of life. This requires truths about what can truly matter and pure transformations of them, namely memory, denotations, and logic. I don't know all absolute truths, and therefore I only select two common scopes of truths that I believe most people take to make this work applicable to most readers.</p>
+
         <ol>
           <li>
             <p><b className="highlight">Direct senses and widely accepted scientific research</b>. Direct senses are six senses (taste, smell, vision, hearing, touch, and proprioception); however, they don't seem enough to many. We cannot, for example, always use our senses to determine what is healthy. Therefore, I also add widely accepted scientific research as truths. When both truths conflict, I let science win because I believe it sympathizes with most people's ideas of truths.</p>
           </li>
 
           <li>
-            <p><b className="highlight">Direct senses, widely accepted scientific research, and beliefs</b>. Beliefs are anything beyond direct senses and science. They can be religious beliefs (most notably those of Buddhism, Christianity, Islam, and Hinduism). It probably takes too much time for me to study all of them so I will begin with Buddhism first. When these truths conflict, I will give the following priorities: beliefs &gt; science &gt; direct senses because I believe that is what most people who hold such beliefs do.</p>
+            <p><b className="highlight">Direct senses, widely accepted scientific research, and beliefs</b>. Beliefs are anything beyond direct senses and science. I only select common religious beliefs (like those of Buddhism, Christianity, Islam, and Hinduism). It probably takes too much time for me to study all of them so I will begin with Buddhism first. When these truths conflict, I will give the following priorities: beliefs &gt; science &gt; direct senses because I believe that is what most people who hold such beliefs do.</p>
           </li>
         </ol>
+
+        <p>Lastly, I am going to extend the scope to the societal level. The <i>ideal</i> society satisfies what can truly matter and mediate conflicts (of what can truly matter).</p>
       </li>
 
       <li>
