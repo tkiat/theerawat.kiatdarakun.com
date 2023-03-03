d394a9cbaa7044ea5294adf7551e0900c2a43fb0
tkiat <tkiat@tutanota.com>
2023-03-03 11:56:44 +0700

    UPDATE

--- src/App/component/Content/page/Create/WhatCanTrulyMatter.tsx
+++ src/App/component/Content/page/Create/WhatCanTrulyMatter.tsx
@@ -6,32 +6,32 @@ export const WhatCanTrulyMatter = (): React.ReactElement =>
 
     <p>My contemplative journey to find the meaning of life has culminated in the realization that regardless of the actual reality (be it only mental construct, only material objects, both, or none), what can truly matter cannot go beyond the flow inside sentient beings. Next, I will find truths about such flows and determine the skillful ways to apply these truths in my life, others' lives, and society. This should enable readers to focus on what can truly matter instead of some illusory pursuits.</p>
 
-    <p>However, my perception is limited, and I don't know all absolute truths about all such flows. The best I can do is to enable readers to make good judgment in life based on what can truly matter and their ideas of truths. Even if these works don't attract many readers, I am going to finish them anyway since nothing else truly matters. To sum up, I am doing the following works.</p>
+    <p>Even if these works don't attract many readers, I am going to finish them anyway since nothing else truly matters. To sum up, I am doing the following works.</p>
 
     <ul className="more-gap-list">
       <li>
-        <p><b className="highlight">The meaning of life and what can truly matter</b>. There are many perspectives about the meaning of life, but all of them seem baseless. To escape from this pitfall is to begin with what can truly matter and determine the meaning of life from there. This work investigates what can truly matter.</p>
+        <p><b className="highlight">Discover the meaning of life from what can truly matter</b>. There are many perspectives about the meaning of life, but all of them seem baseless. To escape from this pitfall is to begin with what can truly matter and determine the meaning of life from there. This work investigates the abstract ideas of what can truly matter.</p>
 
-        <p>The next step is to build upon what can truly matter, once realized, to find the meaning of life. This requires truths about what can truly matter and pure transformations of them, namely memory, denotations, and logic. I don't know all absolute truths, and therefore I only select two common scopes of truths that I believe most people take to make this work applicable to most readers.</p>
+        <p>However, the abtract ideas are useless without truths. My perception is limited, and I don't know all such truths. To make my work applicable to most readers, I have to follow their ideas of truths, two scopes of which will be selected.</p>
 
         <ol>
           <li>
-            <p><b className="highlight">Direct senses and widely accepted scientific research</b>. Direct senses are six senses (taste, smell, vision, hearing, touch, and proprioception); however, they don't seem enough to many. We cannot, for example, always use our senses to determine what is healthy. Therefore, I also add widely accepted scientific research as truths. When both truths conflict, I let science win because I believe it sympathizes with most people's ideas of truths.</p>
+            <p><b className="highlight">Direct senses and widely accepted scientific research</b>. Direct senses are six senses (taste, smell, vision, hearing, touch, and proprioception). I also add widely accepted scientific research as truths as direct senses alone don't seem enough to many. When both truths conflict, I let science win because I believe it sympathizes with most people's ideas of truths.</p>
           </li>
 
           <li>
-            <p><b className="highlight">Direct senses, widely accepted scientific research, and beliefs</b>. Beliefs are anything beyond direct senses and science. I only select common religious beliefs (like those of Buddhism, Christianity, Islam, and Hinduism). It probably takes too much time for me to study all of them so I will begin with Buddhism first. When these truths conflict, I will give the following priorities: beliefs &gt; science &gt; direct senses because I believe that is what most people who hold such beliefs do.</p>
+            <p><b className="highlight">Direct senses, widely accepted scientific research, and religious beliefs</b>. It probably takes too much time for me to study all of them so I will begin with Buddhism first. When these truths conflict, I will give the following priorities: religious beliefs &gt; science &gt; direct senses because I believe that is what most people who hold such beliefs do.</p>
           </li>
         </ol>
 
-        <p>Lastly, I am going to extend the scope to the societal level. The <i>ideal</i> society satisfies what can truly matter and mediate conflicts (of what can truly matter).</p>
+        <p>The truths about that can truly matters can even be extended by pure transformations, namely memory, denotations, and logic. I will explore this in detail. Lastly, I am going to extend the scope to the societal level. The <i>ideal</i> society satisfies what can truly matter and mediate conflicts (of what can truly matter).</p>
       </li>
 
       <li>
-        <p><b className="highlight">The exemplary record of my life</b>. I decided to live by what can truly matter based on my idea of truths (direct senses and widely accepted scientific research). I have recorded my consumables, my belongings, my routines, etc., and I will explain the underlying reasons behind each choice.</p>
+        <p><b className="highlight">The exemplary record of my life</b>. I decided to live by what can truly matter based on my idea of truths (which contains direct senses and widely accepted scientific research). I have recorded my consumables, my belongings, my routines, etc., and I will explain the underlying reasons behind each choice.</p>
       </li>
     </ul>
 
-    <p>All works are still in progress.</p>
+    <p>These works are still in progress.</p>
   </>
 
