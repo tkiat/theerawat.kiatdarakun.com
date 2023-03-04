3c9e8c832431ccf230328334c1e0be91ab52f49a
tkiat <tkiat@tutanota.com>
2023-03-04 20:43:05 +0700

    UPDATE

--- src/App/component/Content/page/Create/WhatCanTrulyMatter.tsx
+++ src/App/component/Content/page/Create/WhatCanTrulyMatter.tsx
@@ -10,28 +10,24 @@ export const WhatCanTrulyMatter = (): React.ReactElement =>
 
     <ul className="more-gap-list">
       <li>
-        <p><b className="highlight">Discover the meaning of life from what can truly matter</b>. There are many perspectives about the meaning of life, but all of them seem baseless. To escape from this pitfall is to begin with what can truly matter and determine the meaning of life from there. This work investigates the abstract ideas of what can truly matter.</p>
+        <p><b className="highlight">Examine the meaning of life from what can truly matter</b>. There are many perspectives about the meaning of life, but all of them seem baseless. To escape from this pitfall is to begin with what can truly matter and examine the meaning of life from there. I will first work examine the abstract ideas of what can truly matter.</p>
 
-        <p>However, the abtract ideas are useless without truths. My perception is limited, and I don't know all such truths. To make my work applicable to most readers, I have to follow their ideas of truths, two scopes of which will be selected.</p>
+        <p>However, the abtract ideas are useless without truths. My perception is limited, and I don't know all truths. To make my work applicable to most readers, I have to follow their ideas of truths, two scopes of which will be selected (direct senses and widely accepted scientific research, with and without religious beliefs).</p>
 
-        <ol>
-          <li>
-            <p><b className="highlight">Direct senses and widely accepted scientific research</b>. Direct senses are six senses (taste, smell, vision, hearing, touch, and proprioception). I also add widely accepted scientific research as truths as direct senses alone don't seem enough to many. When both truths conflict, I let science win because I believe it sympathizes with most people's ideas of truths.</p>
-          </li>
-
-          <li>
-            <p><b className="highlight">Direct senses, widely accepted scientific research, and religious beliefs</b>. It probably takes too much time for me to study all of them so I will begin with Buddhism first. When these truths conflict, I will give the following priorities: religious beliefs &gt; science &gt; direct senses because I believe that is what most people who hold such beliefs do.</p>
-          </li>
-        </ol>
-
-        <p>The truths about that can truly matters can be extended by pure transformations, namely memory, denotations, and logic. I will explore this in detail. Lastly, I am going to extend the scope to the societal level. The <i>ideal</i> society satisfies what can truly matter and mediate conflicts (of what can truly matter).</p>
+        <p>The truths about what can truly matters can be extended by pure transformations, namely memory, denotations, and logic. I will explore this in detail. Next, I will write about the judgment about how to apply such truths in an individual's life. Finally, I will examine the meaning of life based on what I have done so far.</p>
       </li>
 
       <li>
-        <p><b className="highlight">The exemplary record of my life</b>. I decided to live by what can truly matter based on my idea of truths (which contains direct senses and widely accepted scientific research). I have recorded my consumables, my belongings, my routines, etc., and I will explain the underlying reasons behind each choice.</p>
+        <p><b className="highlight">Create my life from what can truly matter</b>. The previous work deals with life in a general sense. This work is, however, my specific interpretion of my idea of truths (namely direct senses and widely accepted scientific research) about what can truly matter. I have recorded my consumables, my belongings, my routines, etc., and I will explain the underlying reasons behind each choice.</p>
+      </li>
+
+      <li>
+        <p><b className="highlight">Create the society from what can truly matter</b>. First, I find truths about what can truly matter within a selected scope of society. Next, I will examine the <i>ideal</i> society, which satisfies such truths and mediate possible conflicts (of such truths). For now, I will consider only one possible scope of society, i.e., all sentient beings. I might consider other scopes, e.g., a muslim society and humanity, if time permits.</p>
       </li>
     </ul>
 
     <p>These works are still in progress.</p>
   </>
 
+//             <p><b className="highlight">Direct senses and widely accepted scientific research</b>. Direct senses are six senses (taste, smell, vision, hearing, touch, and proprioception). I also add widely accepted scientific research as truths as direct senses alone don't seem enough to many. When both truths conflict, I let science win because I believe it sympathizes with most people's ideas of truths.</p>
+//             <p><b className="highlight">Direct senses, widely accepted scientific research, and religious beliefs</b>. It probably takes too much time for me to study all of them so I will begin with Buddhism first. When these truths conflict, I will give the following priorities: religious beliefs &gt; science &gt; direct senses because I believe that is what most people who hold such beliefs do.</p>
