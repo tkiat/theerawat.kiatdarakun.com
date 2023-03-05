d4e16b6a996e6a4ef2a4c88e6db5eb5d677ca079
tkiat <tkiat@tutanota.com>
2023-03-05 16:24:39 +0700

    UPDATE

--- src/App/component/Content/page/Create/WhatCanTrulyMatter.tsx
+++ src/App/component/Content/page/Create/WhatCanTrulyMatter.tsx
@@ -4,28 +4,25 @@ export const WhatCanTrulyMatter = (): React.ReactElement =>
   <>
     <h2>What Can Truly Matter</h2>
 
-    <p>My contemplative journey to find the meaning of life has culminated in the realization that what can truly matter cannot go beyond the flows inside sentient beings regardless of the actual reality. I am now working on the examination of the truths about such flows to enable readers to focus on what can truly matter instead of some illusory pursuits. Even if these works don't attract many readers, I am going to do the following works anyway since nothing else truly matters.</p>
+    <p>My abstract, contemplative journey to find the meaning of life has culminated in the realization that all meanings are baseless unless it is derived from what can truly matter, which cannot go beyond the flows inside sentient beings regardless of the actual reality. I regard the wisest ways of life as the ones derived from no more than such flows, and I am doing the following works that describe such ways of life to enable the readers to focus on what can truly matter instead of illusory pursuits.</p>
+
+    <p><b className="highlight">What can truly matter?</b>. I will begin with the empty universe where nothing matters and gradually adds lives into the picture to find the abstract description of what can truly matter, which will become the foundations of the following aspirations.</p>
 
     <ul className="more-gap-list">
       <li>
-        <p><b className="highlight">Discover the meaning of life from what can truly matter</b>. There are many perspectives about the meaning of life, but all of them seem baseless. To escape from this pitfall, I will begin with the abstract ideas of what can truly matter (this will be the flows inherent to sentient beings) and use that to discover the meaning of life.</p>
+        <p><b className="highlight">Discover the meaning of an individual's life</b>. The abstract description of what can truly matter is useless without truths. My capabilities are limited, and I don't know every truth. To make my work applicable to most readers, I have to follow their ideas of truths, two scopes of which will be selected (direct senses and widely accepted scientific research, with and without religious beliefs).</p>
 
-        <p>However, abstract ideas are useless without truths. My capabilities are limited, and I don't know every truth. To make my work applicable to most readers, I have to follow their ideas of truths, two scopes of which will be selected (direct senses and widely accepted scientific research, with and without religious beliefs).</p>
-
-        <p>The truths about what can truly matters can then be extended by pure transformations, namely memory, denotations, and logic. I will explore this in detail. Next, I will determine the applications of such truths in an individual's life. Finally, I will use all truths and their applications to discover the meaning of life.</p>
+        <p>The truths about what can truly matter can then be extended by pure transformations, namely memory, denotations, and logic. I will explore this in detail. Finally, I will use such truths, along with the judgment about them, to discover the meaning of life.</p>
       </li>
 
       <li>
-        <p><b className="highlight">Create my life from what can truly matter</b>. This will serve as one practical example of how to live life by what can truly matter. I will apply my idea of truths (namely direct senses and widely accepted scientific research) about what can truly matter to my life. I have recorded my consumables, my belongings, my routines, etc., and I will explain the underlying reasons behind each choice.</p>
+        <p><b className="highlight">Make a record of my life</b>. I will make judgments about all my life choices based on my ideas of truths (namely direct senses and widely accepted scientific research) about what can truly matter. The record of such choices (e.g., consumables, belongings, routines) and their underlying reasons will ultimately serve as one practical example of what it means by living by what can truly matter.</p>
       </li>
 
       <li>
-        <p><b className="highlight">Find the ideal society from what can truly matter</b>. This can be done by examining the qualities of the society that can satisfy the truths about what can truly matter within a selected scope. I will begin with the biggest possible scope of society (i.e., all sentient beings) and, if time permits, I will do some other scopes as well (e.g., muslim society and humanity).</p>
+        <p><b className="highlight">Describe the ideal society</b>. This can be done by examining the qualities of the society that can satisfy the truths about what can truly matter within the members of society. I will begin with the biggest possible scope of society (i.e., all sentient beings) and, if time permits, I will do some other scopes as well (e.g., Muslim society and humanity).</p>
       </li>
     </ul>
 
     <p>These works are still in progress.</p>
   </>
-
-//             <p><b className="highlight">Direct senses and widely accepted scientific research</b>. Direct senses are six senses (taste, smell, vision, hearing, touch, and proprioception). I also add widely accepted scientific research as truths as direct senses alone don't seem enough to many. When both truths conflict, I let science win because I believe it sympathizes with most people's ideas of truths.</p>
-//             <p><b className="highlight">Direct senses, widely accepted scientific research, and religious beliefs</b>. It probably takes too much time for me to study all of them so I will begin with Buddhism first. When these truths conflict, I will give the following priorities: religious beliefs &gt; science &gt; direct senses because I believe that is what most people who hold such beliefs do.</p>
