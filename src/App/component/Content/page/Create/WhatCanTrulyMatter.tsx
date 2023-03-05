22c0d67e50034fe5c35326be389ed639c0108205
tkiat <tkiat@tutanota.com>
2023-03-05 11:50:12 +0700

    UPDATE

--- src/App/component/Content/page/Create/WhatCanTrulyMatter.tsx
+++ src/App/component/Content/page/Create/WhatCanTrulyMatter.tsx
@@ -4,17 +4,15 @@ export const WhatCanTrulyMatter = (): React.ReactElement =>
   <>
     <h2>What Can Truly Matter</h2>
 
-    <p>My contemplative journey to find the meaning of life has culminated in the realization that regardless of the actual reality (be it only mental construct, only material objects, both, or none), what can truly matter cannot go beyond the flow inside sentient beings. Next, I will find truths about such flows and determine the skillful ways to apply these truths in my life, others' lives, and society. This should enable readers to focus on what can truly matter instead of some illusory pursuits.</p>
-
-    <p>Even if these works don't attract many readers, I am going to finish them anyway since nothing else truly matters. To sum up, I am doing the following works.</p>
+    <p>My contemplative journey to find the meaning of life has culminated in the realization that what can truly matter cannot go beyond the flows inside sentient beings regardless of the actual reality. I am now working on the examination of the truths about such flows to enable readers to focus on what can truly matter instead of some illusory pursuits. Even if these works don't attract many readers, I am going to do the following works anyway since nothing else truly matters.</p>
 
     <ul className="more-gap-list">
       <li>
-        <p><b className="highlight">Find the meaning of life from what can truly matter</b>. There are many perspectives about the meaning of life, but all of them seem baseless. To escape from this pitfall is to begin with what can truly matter and find the meaning of life from there. First, I will determine the abstract ideas of what can truly matter.</p>
+        <p><b className="highlight">Discover the meaning of life from what can truly matter</b>. There are many perspectives about the meaning of life, but all of them seem baseless. To escape from this pitfall, I will begin with the abstract ideas of what can truly matter (this will be the flows inherent to sentient beings) and use that to discover the meaning of life.</p>
 
-        <p>However, the abtract ideas are useless without truths. My perception is limited, and I don't know all truths. To make my work applicable to most readers, I have to follow their ideas of truths, two scopes of which will be selected (direct senses and widely accepted scientific research, with and without religious beliefs).</p>
+        <p>However, abstract ideas are useless without truths. My capabilities are limited, and I don't know every truth. To make my work applicable to most readers, I have to follow their ideas of truths, two scopes of which will be selected (direct senses and widely accepted scientific research, with and without religious beliefs).</p>
 
-        <p>The truths about what can truly matters can be extended by pure transformations, namely memory, denotations, and logic. I will explore this in detail. Next, I will write about the judgment about how to apply such truths in an individual's life. Finally, I will find the meaning of life from what I have done so far.</p>
+        <p>The truths about what can truly matters can then be extended by pure transformations, namely memory, denotations, and logic. I will explore this in detail. Next, I will determine the applications of such truths in an individual's life. Finally, I will use all truths and their applications to discover the meaning of life.</p>
       </li>
 
       <li>
@@ -22,7 +20,7 @@ export const WhatCanTrulyMatter = (): React.ReactElement =>
       </li>
 
       <li>
-        <p><b className="highlight">Find the ideal society from what can truly matter</b>. This can be done by examining the qualities of the society that can satisfy the truths about what can truly matter within a selected scope. A possible scope can be, e.g., a muslim society, humanity, or all sentient beings on earth. I will begin with the all sentient beings and, if time permits, some other scopes as well.</p>
+        <p><b className="highlight">Find the ideal society from what can truly matter</b>. This can be done by examining the qualities of the society that can satisfy the truths about what can truly matter within a selected scope. I will begin with the biggest possible scope of society (i.e., all sentient beings) and, if time permits, I will do some other scopes as well (e.g., muslim society and humanity).</p>
       </li>
     </ul>
 
