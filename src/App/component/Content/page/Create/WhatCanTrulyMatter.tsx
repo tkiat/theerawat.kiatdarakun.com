dd2fe563040061c4a61a2417e8c1b787744e5a29
tkiat <tkiat@tutanota.com>
2023-03-07 12:32:19 +0700

    UPDATE

--- src/App/component/Content/page/Create/WhatCanTrulyMatter.tsx
+++ src/App/component/Content/page/Create/WhatCanTrulyMatter.tsx
@@ -10,17 +10,17 @@ export const WhatCanTrulyMatter = (): React.ReactElement =>
 
     <ul className="more-gap-list">
       <li>
-        <p><b className="highlight">Discover the meaning of life</b>. The abstract description of what can truly matter is useless without truths. My capabilities are limited, and I don't know every truth. To make my work applicable to most readers, I have to follow their ideas of truths, two scopes of which will be selected (direct senses and widely accepted scientific research, with and without religious beliefs).</p>
+        <p><b className="highlight">Attempt to discover the meaning of life</b>. The abstract description of what can truly matter is useless without truths. My capabilities are limited, and I don't know every truth. To make my work applicable to most readers, I have to follow their ideas of truths, two scopes of which will be selected (direct senses and widely accepted scientific research, with and without religious beliefs).</p>
 
-        <p>The truths about what can truly matter can then be extended by pure transformations, namely memory, denotations, and logic. I will explore this in detail. Finally, I will use such truths, along with the judgment about them, to discover the meaning of life.</p>
-      </li>
-
-      <li>
-        <p><b className="highlight">Keep a record of my life</b>. I will make judgments about all my life choices based on my ideas of truths (namely direct senses and widely accepted scientific research) about what can truly matter. The record of such choices (e.g., consumables, belongings, routines) and their underlying reasons will ultimately serve as one practical example of what it means by living by what can truly matter.</p>
+        <p>The truths about what can truly matter can then be extended by pure transformations, namely memory, denotations, and logic. I will explore this in detail. Finally, I will use such truths, along with the judgment about them, in an attempt to discover the meaning of life.</p>
       </li>
 
       <li>
         <p><b className="highlight">Describe the ideal society</b>. This can be done by examining the qualities of the society that can satisfy the truths about what can truly matter within the members of society. I will begin with the biggest possible scope of society (i.e., all sentient beings) and, if time permits, I will do some other scopes as well (e.g., Muslim society and humanity).</p>
+      </li>
+
+      <li>
+        <p><b className="highlight">Record my life</b>. I will make judgments about all my life choices based on my ideas of truths (namely direct senses and widely accepted scientific research) about what can truly matter. The record of such choices (e.g., consumables, belongings, routines) and their underlying reasons will ultimately serve as one practical example of what it means by living by what can truly matter.</p>
       </li>
     </ul>
 
