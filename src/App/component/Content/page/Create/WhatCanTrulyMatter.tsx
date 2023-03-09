6c51620de207f9b6c39b06a09bd814129a3724cf
tkiat <tkiat@tutanota.com>
2023-03-09 13:10:41 +0700

    UPDATE

--- src/App/component/Content/page/Create/WhatCanTrulyMatter.tsx
+++ src/App/component/Content/page/Create/WhatCanTrulyMatter.tsx
@@ -1,28 +1,38 @@
 import React from "react"
 
+import {TooltipFa} from "../../share"
+
 export const WhatCanTrulyMatter = (): React.ReactElement =>
   <>
     <h2>What Can Truly Matter</h2>
 
-    <p>My abstract, contemplative journey to find the meaning of life has culminated in the realization that all meanings are baseless unless it is derived from what can truly matter, which cannot go beyond the flows inside sentient beings regardless of the actual nature of reality. Next, I am going to finish the following works to enable the readers to focus on what can truly matter instead of illusory pursuits.</p>
+    <p>My abstract, contemplative journey to find the true meaning of life has culminated in the realization that any meaning is made up unless it is derived from what can truly matter, which cannot go beyond the flows inherent to sentient beings regardless of the actual nature of reality. I aspire true meaning, and I am going to finish the following works to enable the readers to focus on what can truly matter instead of illusory pursuits.</p>
 
-    <p><b className="highlight">What can truly matter?</b> I will begin with the empty universe where nothing matters and gradually adds lives into the picture to find the abstract description of what can truly matter, which will become the foundations of the following aspirations.</p>
+    <div>
+      Only <b className="highlight">What Can Truly Matter</b>&nbsp;
+      <TooltipFa faclass="fa-solid fa-circle-info">
+        <p>The most fundamental question about meaning (of life or whatever) is not about truths but it is “what can matter?”, for we should stop asking if nothing can matter. The question becomes “What can truly matter” out of my subjective wish to search for truths, which will become the basis of my subsequent works. In this work, I will begin with the empty universe where nothing can matter and gradually adds lives into the picture to find the abstract description of what can truly matter.</p>
+      </TooltipFa>
+    </div>
 
-    <ul className="more-gap-list">
-      <li>
-        <p><b className="highlight">Attempt to discover the meaning of life</b>. The abstract description of what can truly matter is useless without truths. My capabilities are limited, and I don't know every truth. To make my work applicable to most readers, I have to follow their ideas of truths, two scopes of which will be selected (direct senses and widely accepted scientific research, with and without religious beliefs).</p>
+    <div>
+      Can Lead to <b className="highlight">True Meaning of Life</b>&nbsp;
+      <TooltipFa faclass="fa-solid fa-circle-info">
+        <p>This work is built on top of the previous work, namely the abstract description of what can truly matter. However, every abstract thing is useless without truths. My capabilities are limited, and I don't know every truth. To make my work applicable to most readers, I have to follow their common sources of truths, i.e., direct senses, widely accepted scientific research, religious beliefs.</p>
 
-        <p>The truths about what can truly matter can then be extended by pure transformations, namely memory, denotations, and logic. I will explore this in detail. Finally, I will use such truths, along with the judgment about them, in an attempt to discover the meaning of life.</p>
-      </li>
+        <p>The truths about what can truly matter can then be extended by pure transformations, namely memory, denotations, and logic. I will use the extended truths, along with the judgment about them, in an attempt to find the meaning of life.</p>
 
-      <li>
-        <p><b className="highlight">Describe the ideal society</b>. This can be done by examining the qualities of the society that can satisfy the truths about what can truly matter within the members of society. I will begin with the biggest possible scope of society (i.e., all sentient beings) and, if time permits, I will do some other scopes as well (e.g., Muslim society and humanity).</p>
-      </li>
+        <p>Finally, I will proide the record of my choices (e.g., consumables, belongings, routines) and the underlying reason for them, which is the result of my judgments and interpretations of two sources of truths of my choice (namely direct senses and widely accepted scientific research) about what can truly matter. This record will ultimately serve as one practical example of the life with constant, authentic pursuits of true meaning.</p>
+      </TooltipFa>
+    </div>
 
-      <li>
-        <p><b className="highlight">Record my life</b>. I will make judgments about all my life choices based on my ideas of truths (namely direct senses and widely accepted scientific research) about what can truly matter. The record of such choices (e.g., consumables, belongings, routines) and their underlying reasons will ultimately serve as one practical example of what it means by living by what can truly matter.</p>
-      </li>
-    </ul>
+    <div>
+      And <b className="highlight">The Society That Truly Serves Members</b>&nbsp;
+      TODO
+      <TooltipFa faclass="fa-solid fa-circle-info">
+        <p>This can be done by examining the qualities of the society that can satisfy the truths about what can truly matter within the members of society. I will begin with the biggest possible scope of society (i.e., all sentient beings) and, if time permits, I will do some other scopes as well (e.g., Muslim society and humanity).</p>
+      </TooltipFa>
+    </div>
 
     <p>These works are still in progress.</p>
   </>
