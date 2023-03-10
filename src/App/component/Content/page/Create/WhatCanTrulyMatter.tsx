6e79e6ea62c6ed6404e749d5101aa8119b5c49a8
tkiat <tkiat@tutanota.com>
2023-03-11 00:44:07 +0700

    UPDATE

--- src/App/component/Content/page/Create/WhatCanTrulyMatter.tsx
+++ src/App/component/Content/page/Create/WhatCanTrulyMatter.tsx
@@ -6,30 +6,28 @@ export const WhatCanTrulyMatter = (): React.ReactElement =>
   <>
     <h2>What Can Truly Matter</h2>
 
-    <p>My abstract, contemplative journey to find the true meaning of life has culminated in the realization that any meaning is made up unless it is derived from what can truly matter, which cannot go beyond the flows inherent to sentient beings regardless of the actual nature of reality. I aspire true meaning, and I am going to finish the following works to enable the readers to focus on what can truly matter instead of illusory pursuits.</p>
+    <p>My abstract, contemplative journey to find the true meaning of life has culminated in the realization that any meaning is made up unless it is derived from what can truly matter, which cannot go beyond the flows inherent to sentient beings regardless of the actual nature of reality. I aspire true meaning, and therefore I am going to finish (and live by) the following works instead of illusory pursuits.</p>
 
     <div>
-      Only <b className="highlight">What Can Truly Matter</b>&nbsp;
+      Only <b className="highlight">Concrete Truths About What Can Truly Matter</b>&nbsp;
       <TooltipFa faclass="fa-solid fa-circle-info">
-        <p>The most basic entities that constitute any meaning (of life or whatever) are things that can matter, and therefore we need to seek them first before finding any meaning. Out of my subjective wish to search for truths, what I seek becomes “what can truly matter”, which will become the basis of my subsequent works. In this work, I will begin with the empty universe where nothing can matter and gradually adds lives into the picture to find the abstract description of what can truly matter.</p>
+        <p>In this work, I make arguments for what should be considered as the abstract description of what can truly matter. However, it is useless without concrete truths. My capabilities are limited, and I don't know every truth. Therefore, I will follow some common sources of truths, e.g., six senses, scientific research, and religious beliefs. These truths, along with pure transformations (namely denotation, memory, and logic), will become the basis of my subsequent works.</p>
       </TooltipFa>
     </div>
 
     <div>
       Can Lead to <b className="highlight">True Meaning of Life</b>&nbsp;
       <TooltipFa faclass="fa-solid fa-circle-info">
-        <p>The previous work, namely the abstract description of what can truly matter, is useless without concrete truths. My capabilities are limited, and I don't know every truth. To make my work applicable to most readers, I have to follow their common sources of truths, i.e., some of direct senses, widely accepted scientific research, and religious beliefs.</p>
+        <p>I will make judgment on truths about what can truly matter from the earlier work. When I cannot extend such truths anymore, it is then time to examine the possibilities of what can be true meaning of life. I will also compare them with existing literature.</p>
 
-        <p>The concrete truths about what can truly matter can then be extended by pure transformations, namely memory, denotations, and logic. I will use these extended truths, along with the judgment about them, in an attempt to find the meaning of life.</p>
-
-        <p>Finally, I will provide the record of my choices (e.g., consumables, belongings, routines) and the underlying reason for them, which is the result of my judgments and interpretations of two sources of concrete truths of my choice (namely direct senses and widely accepted scientific research) about what can truly matter. This record will ultimately serve as one practical example of the life with constant, authentic pursuits of true meaning.</p>
+        <p>Additionally, I will provide the record of my life that serves as one practical example of the life with constant, authentic pursuits of true meaning. This record contains my choices (e.g., consumables, belongings, routines) and the underlying reason for them, which is the result of my judgments and interpretations of my ideas of concrete truths about what can truly matter.</p>
       </TooltipFa>
     </div>
 
     <div>
-      And <b className="highlight">The Society That Truly Serves Members</b>&nbsp;
+      And <b className="highlight">Society That Truly Serves Members</b>&nbsp;
       <TooltipFa faclass="fa-solid fa-circle-info">
-        <p>Such society satisfies the truths about what can truly matter within its members. I will examine the qualities of such society, beginning from one possible scope of society (i.e., all human beings). Then, I will examine how such society tackles common issues such as media, food &amp; drinks, techonology, human rights, etc. If time permits, I will do some other scopes as well (e.g., Muslim society and Thai people).</p>
+        <p>Such society conforms to the concrete truths about what can truly matter within its members. I will examine the qualities of such society and how it tackles common issues such as media, food &amp; drinks, human rights, etc.</p>
       </TooltipFa>
     </div>
 
