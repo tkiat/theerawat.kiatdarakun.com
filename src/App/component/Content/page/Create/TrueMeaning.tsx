e7cb01ba3757f29cda5f11fb82ae13d11b9937c1
tkiat <tkiat@tutanota.com>
2023-03-19 20:26:12 +0700

    UPDATE

--- src/App/component/Content/page/Create/TrueMeaning.tsx
+++ src/App/component/Content/page/Create/TrueMeaning.tsx
@@ -6,49 +6,23 @@ export const TrueMeaning = (): React.ReactElement =>
   <>
     <h2>The True Meaning</h2>
 
-    <p>The passage of time forced me to either commit suicide or live and decide how to live, and my subjective self inspired me to live by true meaning.
+    <p>The passage of time forced me to either commit suicide or live and decide how to live, and my subjective self inspired me to live by true meaning. I wish to produce works about the true meaning in both individual and societal scales. These works are still in progress.</p>
 
-    Later, my abstract, contemplative journey to find the true meaning culminated in the realization that any meaning is true if and only if it is derived from things that can truly matter, which cannot go beyond sentience that is not created out of thin air, including things that support or oppose it. I am going to finish (and live by) the following works.</p>
+    <h3 className="highlight">The True Meaning of Life</h3>
 
-    <div>
-      I aspire to live by the true meaning, and I don't know any absolute truth. Therefore, I can only live the least illusory path where I minimize certainly illusory things, i.e., made-up things without any basis. True meaning requires truths and meaning (i.e., importance). Both of them are in the eye of the beholder. However, the least illusory path requires these&nbsp;
+    <div>True meaning requires truths and meaning, and both of them are in the eye of the beholder. Stumbling upon the vagueness of the terms, I started my abstract, contemplative journey to find the more specific explanation. This culminated in the realization that one may not know any absolute truth; however, one can instead live the least illusory path where he/she minimizes certainly illusory things, i.e., made-up things without any basis. This path requires these&nbsp;
       <TooltipText text="definitions">
         <ul>
           <li><b className="highlight">Truths</b>. Truths are everything that is not made up. A theist who has Christian faith may regard the bible as a source of truth, while some atheists may regard it as made-up texts, hence not a source of truth. Common sources of truth that many people hold include sensory perception and widely-accepted scientific research.</li>
-          <li><b className="highlight">Importance</b>. Important things are everything that is neither made up nor without any basis, and only subjective consciousness that can arise on its own can satisfy these criteria.</li>
+          <li><b className="highlight">Meaning (i.e., importance)</b>. Important things are everything that is neither made up nor without any basis, and only subjective consciousness that can arise on its own can satisfy these criteria.</li>
         </ul>
-      </TooltipText>.
+      </TooltipText> of truths and meaning.
     </div>
 
-    <p>One may extend truths through pure transformations (like denotation and logic) and importance by discovering what can support or oppose important things. The true meaning of life cannot go beyond such extensions.</p>
+    <p>In this work, I will firstly describe abstractly truths and meaning, then I will do the same but in an concrete way. Then, I will extend truths and meaning. Next, I will make judgments on extended truths and meaning. When the extensions seem to reach the limit, I will examine the possibilities of what can be the true meaning of life and also compare them with existing literature.</p>
+    <p>Additionally, I will provide a record of my life that serves as one practical example of life with constant, authentic pursuits of true meaning. This record contains my choices (e.g., consumables, belongings, and routines) and the underlying reason for them.</p>
 
+    <h3 className="highlight">Society That Truly Serves Member</h3>
 
-
-
-
-
-    <div>
-      Only <b className="highlight">Concrete Truths About Things That Can Truly Matter</b>&nbsp;
-      <TooltipFa faclass="fa-solid fa-circle-info">
-        <p>In this work, I make arguments for what should be considered as the abstract description of things that can truly matter. However, it is useless in real life without concrete truths. My capabilities are limited, and I don't know every truth. Therefore, I will follow some common sources of truth, e.g., the six senses, scientific research, and religious beliefs. These truths, along with pure transformations of them (namely denotation, memory, and logic), will become the bases of my subsequent works.</p>
-      </TooltipFa>
-    </div>
-
-    <div>
-      Can Lead to <b className="highlight">True Meaning of Life</b>&nbsp;
-      <TooltipFa faclass="fa-solid fa-circle-info">
-        <p>I will make judgments on truths about things that can truly matter from the earlier work. When I cannot extend such truths anymore, it is then time to examine the possibilities of what can be the true meaning of life. I will also compare them with existing literature.</p>
-
-        <p>Additionally, I will provide a record of my life that serves as one practical example of life with constant, authentic pursuits of true meaning. This record contains my choices (e.g., consumables, belongings, and routines) and the underlying reason for them, which is the result of my judgments and interpretations of my ideas of concrete truths about things that can truly matter.</p>
-      </TooltipFa>
-    </div>
-
-    <div>
-      And <b className="highlight">Society That Truly Serves Members</b>&nbsp;
-      <TooltipFa faclass="fa-solid fa-circle-info">
-        <p>Such society conforms to the concrete truths about things that can truly matter within its members. I will examine the qualities of such society and how it tackles common issues such as media, food &amp; drinks, human rights, etc.</p>
-      </TooltipFa>
-    </div>
-
-    <p>These works are still in progress.</p>
+    <p>A fully functional society conforms to the true meaning of life (from the earlier work) that may vary among its members. In this work, I will examine the qualities of such society and how it tackles common issues such as media, food &amp; drinks, human rights, etc.</p>
   </>
