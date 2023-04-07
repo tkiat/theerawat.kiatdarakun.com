f452633f769c3658a068090719d33f3d620655cc
tkiat <tkiat@tutanota.com>
2023-04-08 00:20:50 +0700

    UPDATE

--- src/App/component/Content/page/Create/TrueMeaning.tsx
+++ src/App/component/Content/page/Create/TrueMeaning.tsx
@@ -6,39 +6,40 @@ export const TrueMeaning = (): React.ReactElement =>
   <>
     <h2>The True Meaning</h2>
 
-    <p>The passage of time forced me to either commit suicide or live (and optionally decide how to live), and my subjective self inspired me to live by true meaning. Based on the broadest definition of&nbsp;
+    <p>The passage of time forced me to either commit suicide or live (and optionally decide how to live), and my subjective self inspired me to live by true meaning. Based on the definitions of&nbsp;
       <TooltipText text="meaning">
-        Only subjective consciousness may have meaning (i.e., a sense of importance) <i>on its own</i> as the universe without such cannot have any meaning. Therefore, in the broadest sense, subjective consciousness, together with things that can support or oppose it, may have meaning.
+        Meaning is a sense of importance, which requires a <i>conscious</i> inclination toward or against something. It seems only emotions, sensory experiences, and thoughts can create meaning.
       </TooltipText>
-      , I wish to produce works about the&nbsp;
-      <TooltipText text="true meaning">
-        True meaning is meaning bounded by truths. In the broadest sense, subjective consciousness that reflects truths, together with things that can support or oppose it, may have true meaning. What are truths? While one may know nothing (other than one's untainted conscious experience), one can be at least certain that it is false to mistake a conscious experience for another, e.g., imagination as a physical quality, memory as feeling, and sensory perception plus thought as another sensory perception.
+      &nbsp;and&nbsp;
+      <TooltipText text="meaning of life">
+        Meaning of life is a sense of importance associated with life, which may be perceived by any capable entity (this can be that life itself or one or more other entities). The definitions of life and other entities depend on knowledge and beliefs: some believe in souls; some believe in God; some believe only in their ideas of the physical world; some believe that only their consciousness exists.
+      </TooltipText>
+      , I wish to produce works about&nbsp;
+      <TooltipText text="the true meaning of life">
+        Imagine I create a unique, blue unicorn named Bob. Even though Bob is fictional, its true color is blue. Painting it using a different color other than blue makes that a quality of it but that quality is not true. The same applies to the true meaning of life, which is the meaning associated with the essence of life itself, no matter if life is fictional or not. Therefore, to me, meaning created by my emotions or my sensory experiences is always true, while meaning created by my thoughts alone is always untrue.
       </TooltipText>
       &nbsp;on both individual and societal scales. These works are still in progress.</p>
 
     <h3 className="highlight">The True Meaning of Life</h3>
 
     <div>
-      One's judgments and interpretations of true meaning constitute one's true meaning of life. This work (
-      <TooltipText text="outline">
+      This work has three main goals: determine the abstract definition of the true meaning of life, explore concretely some ways of life that align with true meaning and compare them with existing literature, and provide some records of my life that serve as a practical example of the pursuit of the true meaning.&nbsp;
+      <TooltipText text="Outline">
         <ol>
-          <li>Introduction</li>
-
           <li>
-            The Abstract Examination of True Meaning
+            Introduction
             <ol>
-              <li>True Meaning</li>
-              <li>What May Have True Meaning on Its Own</li>
-              <li>What May Not Have True Meaning on Its Own</li>
+              <li>Background</li>
+              <li>The Abstract Examination of the True Meaning of Life</li>
             </ol>
           </li>
 
           <li>
-            The Concrete Examination of True Meaning
+            The Concrete Examination of the True Meaning of Life
             <ol>
-              <li>Sensory Perception</li>
-              <li>Science</li>
-              <li>Beliefs</li>
+              <li>Meaning Through Sensory Perceptions</li>
+              <li>Meaning Through Emotions</li>
+              <li>Meaning Through Beliefs</li>
             </ol>
           </li>
 
@@ -62,10 +63,9 @@ export const TrueMeaning = (): React.ReactElement =>
           </li>
         </ol>
       </TooltipText>
-      ) has three main goals: determine the narrowest definition of true meaning, explore some ways of life that align with true meaning and then compare them with existing literature, and provide my record that serves as a practical example of the pursuit of the true meaning.
     </div>
 
-    <h3 className="highlight">Society That Truly Serves Member</h3>
+    <h3 className="highlight">Society That Truly Serves Members</h3>
 
-    <p>A fully functional society conforms to the true meaning of life (from the earlier work) that may vary among its members. This work has three main goals: examine the qualities of such society and determine how it abstractly satisfies its members and then how it concretely tackles common issues such as media, consumables, human rights, etc.</p>
+    <p>Such society conforms to the true meaning of life (from the earlier work) that may vary among its members. This work has three main goals: examine the qualities of such society, determine how it abstractly satisfies its members, and then determine how it concretely tackles common issues such as media, consumables, human rights, etc.</p>
   </>
