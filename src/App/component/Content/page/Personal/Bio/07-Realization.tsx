de4458dfeffffd504759394fc4724b10317160f6
tkiat <tkiat@tutanota.com>
2023-08-03 12:32:25 +0700

    UPDATE

--- src/App/component/Content/page/Personal/Bio/07-Realization.tsx
+++ src/App/component/Content/page/Personal/Bio/07-Realization.tsx
@@ -4,34 +4,22 @@ import {TooltipText} from "../../../share"
 
 export const Realization = (): React.ReactElement =>
   <>
-    <p>
-      <b className="highlight">32 Years Old (Satisfactory Realization)</b> — However, I realized the awareness alone cannot solve any&nbsp;
+    <p><b className="highlight">32 Years Old (Satisfactory Realization)</b> — However, I realized the awareness alone cannot solve any&nbsp;
       <TooltipText text="inevitable conflict">
-        Sure, my awareness tells me to change my position when I ache, but what about eating? I currently need to harm other sentient beings for food (even vegan food due to insects), which is a conflict since my awareness tells me that all beings want to survive (as, e.g., their bodies heals themselves). Should I commit suicide or continue living?
+        Sure, my awareness tells me to change my position when I ache, but what about eating? I currently need to harm other sentient beings for food (even vegan food due to insects), which is a conflict since my awareness tells me that all beings want to survive. Should I starve to death or continue living?
       </TooltipText>
-      . I then examined everything from scratch.
+      . I then examined everything from scratch, then I finally discovered the most fundamental conceptual unit behind every conscious action, namely "meaning" (i.e., a sense of importance) and "lack of meaning". One may perform a conscious action without any meaning (e.g., wave a hand just for the sake of it) or with some meaning (e.g., post a Youtube video in the hope of becoming famous).</p>
+
+    <p>However, as wisdom is the ability to make a good judgment, it has to be based on things that are evidential rather than illusory. Therefore, a wise person would perform any conscious action based on true meaning, rather than false meaning or no meaning at all. Fortunately, I can perceive truth by using my awareness since it is itself truth.</p>
+
+    <p>I can only so far be aware of my sensual experiences and thoughts, and these are my limited realization of truth. True meaning, from my perspective, is therefore either my sensual experience and thought that is important. My experience of hunger, for example, is true meaning since I actually experience it (truth) and it demands something (meaning).</p>
 
 
 
-      Most fundamentally, one has to subjectively decide to either live or commit suicide, and one who lives may subjectively decide how to live. After a long self-introspection, I realized that my survival instincts nudged me to live and I had a strong passion to live by true meaning.
-    </p>
+     <p> Following the awareness is still the only wise option (as it is evidential unlike mind). However, in the case of the conflict, one has to judge and cat accordingly. One cannot consider himself wise when he makes no judgment. After all, wisdom is about the ability to make a good judgment right?.</p>
 
-    <p>
-      Based on these definitions of&nbsp;
-      <TooltipText text="meaning">
-        Meaning is a sense of importance, which requires a <i>conscious</i> inclination toward or against something. To my knowledge, only instinctual emotions, sensory perceptions, and thoughts can create meaning.
-      </TooltipText>
-      &nbsp;and&nbsp;
-      <TooltipText text="the meaning of life">
-        The meaning of life is a sense of importance that emerges from life, and any capable entity may perceive it. The definition of such an entity depends on knowledge and beliefs. Some believe in souls and/or God; some believe only in their ideas of the physical world; some believe that only their consciousness exists.
-      </TooltipText>
-      , I realized that&nbsp;
-      <TooltipText text="the true meaning of life">
-        Having five legs is a true property of a five-leg unicorn, even though it is fictional. Likewise, the true meaning of life is a sense of importance emerging from the inherent properties of life (whether or not life is fictional), and any capable entity may perceive it.
+    <p>Such judgment needs to be carried out by my mind. It is usually not possible to pinpoint the best judgment (e.g., one cannot absolutely objectively compare a human's worth with a cow's during an emergency rescue), but I often has no choice. I have discovered that the metric that enables good judgments (on a conflict) is "true meaning", i.e., .</p>
 
-        <br /><br />
-
-        To my knowledge, only instinctual emotions, sensory experiences, and thoughts can create meaning, out of which only the first two are the inherent qualities of life since thoughts alone are arbitrary. Based on emotions and sensory perceptions, one may use memories and thoughts to align actions and possessions with meanings emerging from them. Such action, though probably imperfect, is always a manifestation of one's pursuit of true meaning.
-      </TooltipText>
-      &nbsp;can take me out of the darkness of ignorance. I wanted to live by my interpretations and judgments about true meaning of life and produce relevant works.</p>
+    <p>I now conclude that any seeker of wisdom should live by true meaning.</p>
+      &nbsp;can take me out of the darkness of ignorance. I wanted to live by my interpretations and judgments about true meaning of life and produce relevant works.
   </>
