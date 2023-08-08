7459173677e5bca69bdc112961e49562ed090559
tkiat <tkiat@tutanota.com>
2023-08-08 17:16:15 +0700

    UPDATE

--- src/App/component/Content/page/Personal/TrueMeaning/About.tsx
+++ src/App/component/Content/page/Personal/TrueMeaning/About.tsx
@@ -6,65 +6,7 @@ export const About = (): React.ReactElement =>
 
     <p>A long period of self-introspection has culminated in overcoming <i>long</i> analysis paralysis on what I should do in my life. To answer that question, one has to investigate the most fundamental building blocks that motivate every conscious action, namely "meaning" (i.e., a sense of importance) and "lack of meaning". One may perform a conscious action without any meaning (e.g., wave a hand just for the sake of it) or with some meaning (e.g., post something to gain fame).</p>
 
-    <p>One who seeks wisdom (i.e., the ability to make good judgments) needs to base his/her conscious actions based on things that are true rather than illusory. In other words, a wise person would perform any conscious action based on true meaning, rather than false meaning or no meaning at all. My strong wish to become wise inspired me to begin this work (on true meaning). I split this work into two books of different scopes.</p>
+    <p>One who seeks wisdom (i.e., the ability to make good judgments) needs to base his/her conscious actions based on things that are true rather than illusory. In other words, a wise person would perform any conscious action based on true meaning, rather than false meaning or no meaning at all. My strong wish to become wise inspired me to begin this work (on true meaning). I split this work into two books of different scopes: individual and societal scopes.</p>
 
-    <ol>
-      <li>
-        <b>Individual scope</b>. The book focuses on true meaning as perceived by a single entity such as myself, a fellow human, and an animal. It begins with the motivation before diving into the examination of true meaning.
-
-        <p><u>Outline (Subject to Change)</u></p>
-
-        <ol>
-          <li>
-            Introduction
-            <ol>
-              <li>
-                Background
-                <ol>
-                  <li>The Rejection of Nihilism</li>
-                  <li>Wisdom: the Only Quality That is Always Desirable</li>
-                  <li>Wisdom and True Meaning</li>
-                </ol>
-              </li>
-            </ol>
-          </li>
-
-          <li>
-            The Abstract Examination of True Meaning
-            <ol>
-              <li>Truth</li>
-              <li>Meaning</li>
-              <li>True Meaning</li>
-              <li>Existing Literature Through the Lens of True Meaning</li>
-            </ol>
-          </li>
-
-          <li>
-            The Concrete, Non-Judgmental Examination of True Meaning
-            <ol>
-              <li>Nonbelievers</li>
-              <li>Believers</li>
-              <li>Nonhumans</li>
-            </ol>
-          </li>
-
-          <li>
-            The Concrete, Judgmental Examination of True Meaning
-            <ol>
-              <li>Nonbelievers</li>
-              <li>Believers</li>
-              <li>
-                Me
-                <ol>
-                  <li>My Choices</li>
-                  <li>My Records</li>
-                </ol>
-              </li>
-            </ol>
-          </li>
-        </ol>
-      </li>
-
-      <li><p><b>Societal scope</b>. This book investigates the ideal society that enables each member to follow his/her true meaning. I will investigate if I can avoid any compromise in such society and, if not, how to make the best judgment for the best interests of all members. I will determine how the society tackles common issues such as media, consumables, human rights, etc.</p></li>
-    </ol>
+    <p>The first book focuses on true meaning as perceived by a single entity such as myself, a fellow human, and an animal. The second book describes the ideal society that enables each member to follow his/her true meaning by tackling common issues such as media, consumables, human rights, etc. The book also shows how such society resolves the conflicts, if any, for the best interests of all members.</p>
   </>
--- src/App/component/Content/page/Personal/TrueMeaning/LitReview.tsx
+++ src/App/component/Content/page/Personal/TrueMeaning/LitReview.tsx
@@ -4,36 +4,112 @@ import {DisplayListItems} from "../../../share"
 
 export const LitReview = ({content}: {content: unknown}): React.ReactElement =>
   <>
-    <h1>Literature Review</h1>
+    <h1>Outline and Literature Review</h1>
 
-    <h2><u>Individual Scope</u></h2>
+    <p>The outline is subject to change.</p>
 
-    <h3 className="highlight">Metaphysics</h3>
-    <DisplayListItems
-      content={content}
-      keys={["litreview", "individual", "metaphysics"]}
-    />
+    <h2><u>Book #1: Individual Scope</u></h2>
 
-    <h3 className="highlight">Illusions</h3>
+    <ol>
+      <li>
+        Background
+        <ol>
+          <li>The Rejection of Nihilism</li>
+          <li>Wisdom: the Only Quality That is Always Desirable</li>
+          <li>Wisdom and True Meaning</li>
+        </ol>
+      </li>
 
-    <p>Illusions are ideas held by some individuals that contradict the reality.</p>
+      <li>
+        The Abstract Examination of True Meaning
+        <ol>
+          <li>
+            Truth
+            <details>
+              <summary className="highlight"><u>Literature Review</u></summary>
+              <DisplayListItems
+                content={content}
+                keys={["litreview", "individual", "metaphysics"]}
+              />
+            </details>
+          </li>
 
-    <DisplayListItems
-      content={content}
-      keys={["litreview", "individual", "illusions"]}
-    />
+          <li>Meaning</li>
 
-    <h2><u>Societal Scope</u></h2>
+          <li>True Meaning</li>
 
-    <h3 className="highlight">Animal Welfare</h3>
-    <DisplayListItems
-      content={content}
-      keys={["litreview", "society", "animal welfare"]}
-    />
+          <li>Existing Literature Through the Lens of True Meaning</li>
+        </ol>
+      </li>
 
-    <h3 className="highlight">Religion</h3>
-    <DisplayListItems
-      content={content}
-      keys={["litreview", "society", "religion"]}
-    />
+      <li>
+        The Concrete, Non-Judgmental Examination of True Meaning
+        <ol>
+          <li>
+            Humans
+            Nonbelievers
+            <ol>
+              <li>
+                Illusions
+                <details>
+                  <summary className="highlight"><u>Literature Review</u></summary>
+                  <DisplayListItems
+                    content={content}
+                    keys={["litreview", "individual", "illusions"]}
+                  />
+                </details>
+              </li>
+            </ol>
+          </li>
+
+          <li>Believers</li>
+
+          <li>Nonhumans</li>
+        </ol>
+      </li>
+
+      <li>
+        The Concrete, Judgmental Examination of True Meaning
+
+        <ol>
+          <li>Nonbelievers</li>
+          <li>Believers</li>
+          <li>
+            Me
+            <ol>
+              <li>My Choices</li>
+              <li>My Records</li>
+            </ol>
+          </li>
+        </ol>
+      </li>
+    </ol>
+
+    <h2><u>Book #2: Societal Scope</u></h2>
+
+    <p>The outline is yet to be done.</p>
+
+    <ol>
+      <li>
+        Animal Welfare
+        <details>
+          <summary className="highlight"><u>Literature Review</u></summary>
+          <DisplayListItems
+            content={content}
+            keys={["litreview", "society", "animal welfare"]}
+          />
+        </details>
+      </li>
+
+      <li>
+        Religion
+        <details>
+          <summary className="highlight"><u>Literature Review</u></summary>
+          <DisplayListItems
+            content={content}
+            keys={["litreview", "society", "religion"]}
+          />
+        </details>
+      </li>
+    </ol>
   </>
